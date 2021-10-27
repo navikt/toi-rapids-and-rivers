cf19d43bd6d88e20665625c6ba5d3b2ee5807924
Joar Aurdal <joar.aurdal@nav.no>
2021-10-27 15:09:37 +0200

    Legg til nais config

--- apps/toi-veileder-reader/src/main/kotlin/no/nav/arbeidsgiver/toi/veileder/Application.kt
+++ apps/toi-veileder-reader/src/main/kotlin/no/nav/arbeidsgiver/toi/veileder/Application.kt
@@ -9,14 +9,14 @@ fun main() {
     val rapidApplication = RapidApplication.create(System.getenv())
     rapidApplication.start()
 
-    log.info("Rapid-applikasjon er startet. Starter CvLytter")
+    log.info("Rapid-applikasjon er startet. Starter VeilederLytter")
 
     VeilederLytter(
         meldingsPublisher = rapidApplication::publish,
         shutdownRapidApplication = rapidApplication::stop,
         consumerConfig = veilederLytterConfig(System.getenv("TOI_VEILEDER_KAFKA_GROUP"))
     ).start()
-    log.info("CvLytter er startet")
+    log.info("VeilederLytter er startet")
 }
 
 private val log
