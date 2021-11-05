dbceb5d5d3026a661a1646427d2e7886a531ae67
Mads Lee Giil <mads.lee.giil@nav.no>
2021-11-05 11:26:04 +0100

    Test Mongo-tilkobling

--- apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
+++ apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
@@ -1,20 +1,24 @@
 package no.nav.arbeidsgiver.toi
 
-import com.mongodb.client.MongoClients
+import com.mongodb.MongoClient
+import com.mongodb.ServerAddress
 import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 
 fun startApp(repository: Repository) = RapidApplication.create(System.getenv()).also { rapid ->
-//    val behandler =  Behandler( repository, rapid::publish)
-//
-//    VeilederLytter(rapid, behandler)
-//    CvLytter(rapid, behandler)
+    val behandler =  Behandler( repository, rapid::publish)
+
+    VeilederLytter(rapid, behandler)
+    CvLytter(rapid, behandler)
 }.start()
 
-val mongoDbUrl = System.getenv("MONGODB_URL")
-val mongoClient = MongoClients.create(mongoDbUrl)
+val mongoClient = MongoClient(listOf(
+    ServerAddress("toi-sammenstill-kandidat-mongodb-0.toi-sammenstill-kandidat-mongodb", 27017),
+    ServerAddress("toi-sammenstill-kandidat-mongodb-1.toi-sammenstill-kandidat-mongodb", 27017),
+    ServerAddress("toi-sammenstill-kandidat-mongodb-2.toi-sammenstill-kandidat-mongodb", 27017)
+))
 
 fun main() = startApp(Repository(mongoClient))
 
