becb797886aede3e1f3ca9506790146129387721
Mads Lee Giil <mads.lee.giil@nav.no>
2021-11-05 10:39:31 +0100

    Bruk service som host for MongoDB

--- apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
+++ apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
@@ -1,5 +1,7 @@
 package no.nav.arbeidsgiver.toi
 
+import com.mongodb.MongoClient
+import com.mongodb.ServerAddress
 import com.mongodb.client.MongoClients
 import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
@@ -13,8 +15,11 @@ fun startApp(repository: Repository) = RapidApplication.create(System.getenv()).
     CvLytter(rapid, behandler)
 }.start()
 
-val mongoDbUrl = System.getenv("MONGODB_URL")
-val mongoClient = MongoClients.create(mongoDbUrl)
+//val mongoDbUrl = System.getenv("MONGODB_URL")
+//val mongoClient = MongoClients.create(mongoDbUrl)
+
+val mongoClient = MongoClient(listOf(ServerAddress("toi-sammenstill-kandidat-mongodb:27017")))
+
 fun main() = startApp(Repository(mongoClient))
 
 val Any.log: Logger
