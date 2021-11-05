7ebfea65f7c23ce89c289b7377be88d2b3b61422
Mads Lee Giil <mads.lee.giil@nav.no>
2021-11-05 13:44:10 +0100

    Bruk ordinær connectionString

--- apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
+++ apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
@@ -1,11 +1,6 @@
 package no.nav.arbeidsgiver.toi
 
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
-import com.mongodb.ServerAddress
 import com.mongodb.client.MongoClients
-import com.mongodb.connection.ClusterConnectionMode
-import com.mongodb.connection.ClusterType
 import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -17,18 +12,7 @@ fun startApp(repository: Repository) = RapidApplication.create(System.getenv()).
     CvLytter(rapid, behandler)
 }.start()
 
-val mongoSettings = MongoClientSettings.builder()
-    .applyToClusterSettings {
-        it.hosts(listOf(
-            ServerAddress("toi-sammenstill-kandidat-mongodb-2.toi-sammenstill-kandidat-mongodb:27017/deploy_log?replicaSet=MainRepSet"),
-            ServerAddress("toi-sammenstill-kandidat-mongodb-1.toi-sammenstill-kandidat-mongodb:27017"),
-            ServerAddress("toi-sammenstill-kandidat-mongodb-0.toi-sammenstill-kandidat-mongodb:27017")
-        ))
-        it.mode(ClusterConnectionMode.MULTIPLE)
-        it.requiredClusterType(ClusterType.REPLICA_SET)
-    }.build()
-
-val mongoClient = MongoClients.create(mongoSettings)
+val mongoClient = MongoClients.create(System.getenv("MONGODB_URL"))
 
 fun main() = startApp(Repository(mongoClient))
 
