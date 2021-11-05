cbb231ade4b9fd72aef6de6c4e7ad34dce8f79e3
Mads Lee Giil <mads.lee.giil@nav.no>
2021-11-05 12:18:34 +0100

    Test mongodb-tilkobling

--- apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
+++ apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
@@ -1,7 +1,6 @@
 package no.nav.arbeidsgiver.toi
 
 import com.mongodb.MongoClient
-import com.mongodb.ServerAddress
 import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -9,15 +8,11 @@ import org.slf4j.LoggerFactory
 
 fun startApp(repository: Repository) = RapidApplication.create(System.getenv()).also { rapid ->
     val behandler =  Behandler( repository, rapid::publish)
-
     VeilederLytter(rapid, behandler)
     CvLytter(rapid, behandler)
 }.start()
 
-val mongoClient = MongoClient(listOf(
-    ServerAddress("toi-sammenstill-kandidat-mongodb", 27017),
-))
-
+val mongoClient = MongoClient("mongodb://toi-sammenstill-kandidat-mongodb:27017")
 fun main() = startApp(Repository(mongoClient))
 
 val Any.log: Logger
