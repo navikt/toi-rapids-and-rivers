fe553d8b356564fa4865a3ef49f077c57775a118
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-11-09 14:41:31 +0100

    Fjern oppsett av mongoDB

--- apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
+++ apps/toi-sammenstill-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
@@ -1,24 +1,16 @@
 package no.nav.arbeidsgiver.toi
 
-import com.mongodb.MongoClient
-import com.mongodb.ServerAddress
 import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-
-fun startApp(repository: Repository) = RapidApplication.create(System.getenv()).also { rapid ->
-    val behandler =  Behandler( repository, rapid::publish)
-    VeilederLytter(rapid, behandler)
-    CvLytter(rapid, behandler)
+fun startApp() = RapidApplication.create(System.getenv()).also {
+    //val behandler =  Behandler( repository, rapid::publish)
+    //VeilederLytter(rapid, behandler)
+    //CvLytter(rapid, behandler)
 }.start()
 
-val mongoClient = MongoClient(listOf(
-    ServerAddress("toi-sammenstill-kandidat-mongodb-2.toi-sammenstill-kandidat-mongodb", 27017 ),
-    ServerAddress("toi-sammenstill-kandidat-mongodb-1.toi-sammenstill-kandidat-mongodb", 27017),
-    ServerAddress("toi-sammenstill-kandidat-mongodb-0.toi-sammenstill-kandidat-mongodb", 27017)
-))
-fun main() = startApp(Repository(mongoClient))
+fun main() = startApp()
 
 val Any.log: Logger
     get() = LoggerFactory.getLogger(this::class.java)
