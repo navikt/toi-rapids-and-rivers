9b1c3796cc964a01dc4e464ef8329e6a319398cb
Mads Lee Giil <mads.lee.giil@nav.no>
2021-11-10 08:21:08 +0100

    Logg hendelser med stringverdien av JsonMessage

--- apps/toi-sammenstille-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Hendelse.kt
+++ apps/toi-sammenstille-kandidat/src/main/kotlin/no/nav/arbeidsgiver/toi/Hendelse.kt
@@ -4,6 +4,9 @@ import no.nav.helse.rapids_rivers.JsonMessage
 
 data class Hendelse(val hendelseType: HendelseType, val aktørid: String, val jsonMessage: JsonMessage) {
     fun populerKandidat(kandidat: Kandidat) = hendelseType.populerKandidat(kandidat, jsonMessage)
+    override fun toString(): String {
+        return "Hendelse(hendelseType=$hendelseType, aktørid='$aktørid', jsonMessage=${jsonMessage.toJson()})"
+    }
 }
 
 interface HendelseType {
