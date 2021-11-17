ae6ac0fd4830b0b572c33cde8057386ce56c4836
Joar Aurdal <joar.aurdal@nav.no>
2021-11-17 12:49:25 +0100

    Fiks config for toi-kandidat producer

--- apps/toi-kandidatfeed/src/main/kotlin/no/nav/arbeidsgiver/toi/kafkafeed/producerConfig.kt
+++ apps/toi-kandidatfeed/src/main/kotlin/no/nav/arbeidsgiver/toi/kafkafeed/producerConfig.kt
@@ -8,10 +8,6 @@ import org.apache.kafka.common.serialization.StringSerializer
 val producerConfig = mapOf(
     CommonClientConfigs.CLIENT_ID_CONFIG to "toi-kadidatfeed",
     CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG to System.getenv("KAFKA_BROKERS"),
-
-    ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java.name,
-
-
     CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to "SSL",
     SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to System.getenv("KAFKA_KEYSTORE_PATH"),
     SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to System.getenv("KAFKA_CREDSTORE_PASSWORD"),
