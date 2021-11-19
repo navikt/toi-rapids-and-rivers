94a20d1334216ac27e8ee3314948486c4ff3d175
Mads Lee Giil <mads.lee.giil@nav.no>
2021-11-19 13:26:42 +0100

    Endre format på meldinger publisert av toi-veileder

--- apps/toi-veileder/src/main/kotlin/no/nav/arbeidsgiver/toi/veileder/VeilederLytter.kt
+++ apps/toi-veileder/src/main/kotlin/no/nav/arbeidsgiver/toi/veileder/VeilederLytter.kt
@@ -1,24 +1,46 @@
 package no.nav.arbeidsgiver.toi.veileder
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 
-class VeilederLytter(private val rapidsConnection: RapidsConnection): River.PacketListener {
+class VeilederLytter(private val rapidsConnection: RapidsConnection) : River.PacketListener {
     init {
         River(rapidsConnection).apply {
             validate {
                 it.demandKey("aktorId")
                 it.demandKey("veilederId")
                 it.rejectKey("@event_name")
+                it.interestedIn("tilordnet")
             }
         }.register(this)
     }
-    override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        packet["@event_name"] = "veileder"
-        packet["aktørId"] = packet["aktorId"]
 
-        rapidsConnection.publish(packet.toJson())
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        val melding = mapOf(
+            "aktørId" to packet["aktorId"],
+            "veileder" to packet.fjernMetadata(),
+            "@event_name" to "veileder",
+        )
+        val nyPacket = JsonMessage.newMessage(melding)
+        rapidsConnection.publish(nyPacket.toJson())
+    }
+
+    private fun JsonMessage.fjernMetadata(): JsonNode {
+        val jsonNode = jacksonObjectMapper().readTree(this.toJson())
+        val alleFelter = jsonNode.fieldNames().asSequence().toList()
+        val metadataFelter = listOf("system_read_count")
+        val aktuelleFelter = alleFelter.filter { !metadataFelter.contains(it) }
+
+        val rotNode = jacksonObjectMapper().createObjectNode()
+
+        aktuelleFelter.forEach {
+            rotNode.set<JsonNode>(it, jacksonObjectMapper().valueToTree(this[it]))
+        }
+        return rotNode
     }
 }
---
+++ apps/toi-veileder/src/test/kotlin/no/nav/arbeidsgiver/toi/veileder/VeilederTest.kt
@@ -0,0 +1,54 @@
+package no.nav.arbeidsgiver.toi.veileder
+
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class VeilederTest {
+
+    @Test
+    fun `Lesing av veilederMelding fra eksternt topic skal produsere ny melding på rapid`() {
+        val aktørId = "10000100000"
+        val veilederId = "2000020000"
+        val tilordnet = "2020-12-21T10:58:19.023+01:00"
+        val testRapid = TestRapid()
+        VeilederLytter(testRapid)
+
+        testRapid.sendTestMessage(veilederMeldingFraEksterntTopic(aktørId, veilederId, tilordnet))
+        Thread.sleep(300)
+
+        val inspektør = testRapid.inspektør
+
+        assertThat(inspektør.size).isEqualTo(1)
+        val meldingJson = inspektør.message(0)
+
+        assertThat(meldingJson.fieldNames().asSequence().toList()).containsExactlyInAnyOrder(
+            "@event_name",
+            "veileder",
+            "aktørId",
+            "system_read_count"
+        )
+        assertThat(meldingJson.get("@event_name").asText()).isEqualTo("veileder")
+        assertThat(meldingJson.get("aktørId").asText()).isEqualTo(aktørId)
+
+        val veilederJson = meldingJson.get("veileder")
+        assertThat(veilederJson.fieldNames().asSequence().toList()).containsExactlyInAnyOrder(
+            "aktorId",
+            "veilederId",
+            "tilordnet",
+        )
+        meldingJson.get("veileder").apply {
+            assertThat(get("aktorId").asText()).isEqualTo(aktørId)
+            assertThat(get("veilederId").asText()).isEqualTo(veilederId)
+            assertThat(get("tilordnet").asText()).isEqualTo(tilordnet)
+        }
+    }
+
+    private fun veilederMeldingFraEksterntTopic(aktørId: String, veilederId: String, tilordnet: String) = """
+        {
+            "aktorId":"$aktørId",
+            "veilederId":"$veilederId",
+            "tilordnet":"$tilordnet"
+        }
+    """.trimIndent()
+}
