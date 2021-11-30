445ffc0cd5f3c42c1986bc99f4932c5dc6411c7f
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-11-30 08:13:40 +0100

    Deploy identmapper

--- apps/toi-identmapper/src/main/kotlin/no/nav/arbeidsgiver/toi/identmapper/Application.kt
+++ apps/toi-identmapper/src/main/kotlin/no/nav/arbeidsgiver/toi/identmapper/Application.kt
@@ -5,6 +5,6 @@ import no.nav.helse.rapids_rivers.RapidApplication
 fun main() = RapidApplication.create(System.getenv()).also { rapidsConnection ->
     val pdlKlient = PdlKlient()
     listOf("fnr", "fodselsnr", "fodselsnummer").forEach { fnrKey ->
-        AktørIdPopulator(fnrKey, rapidsConnection, pdlKlient::aktørIdFor)
+        // AktørIdPopulator(fnrKey, rapidsConnection, pdlKlient::aktørIdFor)
     }
 }.start()
