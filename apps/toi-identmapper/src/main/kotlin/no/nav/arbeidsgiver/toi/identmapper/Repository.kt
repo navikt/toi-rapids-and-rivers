f6d64ca59611e319508d4f16ee14e95745a73c00
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-12-06 13:11:11 +0100

    Legg til flyway og et init-script

--- apps/toi-identmapper/src/main/kotlin/no/nav/arbeidsgiver/toi/identmapper/Repository.kt
+++ apps/toi-identmapper/src/main/kotlin/no/nav/arbeidsgiver/toi/identmapper/Repository.kt
@@ -1,15 +1,27 @@
 package no.nav.arbeidsgiver.toi.identmapper
 
+import org.flywaydb.core.Flyway
 import java.time.LocalDateTime
 import javax.sql.DataSource
 
-class Repository(dataSource: DataSource) {
+class Repository(private val dataSource: DataSource) {
+    init {
+        kjørFlywayMigreringer()
+    }
+
     fun lagreAktørId(aktørId: String, fødselsnummer: String) {
         TODO("Not yet implemented")
     }
 
     fun hentAktørId(fødselsnummer: String): IdentMapping {
         TODO("Not yet implemented")
+    }
+
+    private fun kjørFlywayMigreringer() {
+        Flyway.configure()
+            .dataSource(dataSource)
+            .load()
+            .migrate()
     }
 }
 
---
+++ apps/toi-identmapper/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE identmapping (
+    aktor_id VARCHAR(20),
+    fnr VARCHAR(20),
+    cachet_tidspunkt TIMESTAMP NOT NULL
+);
