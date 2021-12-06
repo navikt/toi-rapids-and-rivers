3adca21ff3af0fc064a6e4652472e730346654a6
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-12-06 12:24:13 +0100

    Implementer logikk for caching

--- apps/toi-identmapper/src/main/kotlin/no/nav/arbeidsgiver/toi/identmapper/AktorIdCache.kt
+++ apps/toi-identmapper/src/main/kotlin/no/nav/arbeidsgiver/toi/identmapper/AktorIdCache.kt
@@ -2,6 +2,23 @@ package no.nav.arbeidsgiver.toi.identmapper
 
 class AktorIdCache(private val pdlKlient: PdlKlient) {
     fun hentAktørId(fødselsnummer: String): String? {
-        return pdlKlient.hentAktørId(fødselsnummer)
+        var aktørId = hentCachetAktørId(fødselsnummer)
+
+        if (aktørId == null) {
+            aktørId = pdlKlient.hentAktørId(fødselsnummer)
+            cacheAktørId(aktørId)
+        }
+
+        return aktørId
+    }
+
+    private fun cacheAktørId(aktørId: String?) {
+        // TODO: Lagre i cache
+    }
+
+    private fun hentCachetAktørId(fødselsnummer: String): String? {
+        // TODO: Hent cachet aktørId
+        
+        return null
     }
 }
