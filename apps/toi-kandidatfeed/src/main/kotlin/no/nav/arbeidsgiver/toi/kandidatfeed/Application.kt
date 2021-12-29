c3bb1cacfe8ec6eb767264bfae673136d7e47432
Mads Lee Giil <mads.lee.giil@nav.no>
2021-12-29 09:01:12 +0100

    Deaktiver speilet kandidatfeedlytter etter å ha hatt den kjørende lenge nok

--- apps/toi-kandidatfeed/src/main/kotlin/no/nav/arbeidsgiver/toi/kandidatfeed/Application.kt
+++ apps/toi-kandidatfeed/src/main/kotlin/no/nav/arbeidsgiver/toi/kandidatfeed/Application.kt
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory
 
 fun main() = RapidApplication.create(System.getenv()).also { rapidsConnection ->
     KandidatfeedLytter(rapidsConnection, KafkaProducer(producerConfig), erProd())
-    KandidatfeedLytter2(rapidsConnection, KafkaProducer(producerConfig), "toi.sammenstilt-kandidat")
+//    KandidatfeedLytter2(rapidsConnection, KafkaProducer(producerConfig), "toi.sammenstilt-kandidat")
 }.start()
 
 val Any.log: Logger
