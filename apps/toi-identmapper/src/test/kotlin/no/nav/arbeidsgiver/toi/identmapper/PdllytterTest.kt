7fd8bd26b1abfa10b4e3af26d6dd06ff4e1deb68
frodank <11638682+frodank@users.noreply.github.com>
2022-01-14 10:30:59 +0100

    Test for å sjekke om innkommende Aktor-melding blir forsøkt lagret

--- apps/toi-identmapper/src/test/kotlin/no/nav/arbeidsgiver/toi/identmapper/PdllytterTest.kt
+++ apps/toi-identmapper/src/test/kotlin/no/nav/arbeidsgiver/toi/identmapper/PdllytterTest.kt
@@ -1,7 +1,15 @@
 package no.nav.arbeidsgiver.toi.identmapper
 
 import TestDatabase
+import no.nav.arbeidsgiver.toi.cv.PdlLytterConfiguration
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import no.nav.person.pdl.aktor.v2.Aktor
+import no.nav.person.pdl.aktor.v2.Identifikator
+import no.nav.person.pdl.aktor.v2.Type
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.MockConsumer
+import org.apache.kafka.clients.consumer.OffsetResetStrategy
+import org.apache.kafka.common.TopicPartition
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
@@ -16,34 +24,35 @@ class PdllytterTest {
         testDatabase.slettAlt()
     }
 
-    /*
     @Test
     fun `Lesing av pdlMelding fra eksternt topic skal lagres i cache`() {
-        val aktørId = "10000100000"
-        val fnr = "12121233333"
-        val testRapid = TestRapid()
-        PdlLytter(
-            testRapid, this::lagre
-        )
 
-        val pdlMeldingFraEksterntTopic = pdlMeldingFraEksterntTopic(aktørId, fnr)
+        val testAktørId = "123456789"
+        val testFnr = "987654321"
+        var lagreFunksjonHarBlittKalt = false
 
-        testRapid.sendTestMessage(pdlMeldingFraEksterntTopic)
-
-        val inspektør = testRapid.inspektør
-
-        val repository = Repository(testDatabase.dataSource)
-        val aktørIdCache = AktorIdCache(repository, "prod-gcp") { aktørId }
-        assertThat(aktørIdCache.hentAktørId(aktørId)).isEqualTo(aktørId)
-    }
-    */
-
-    private fun pdlMeldingFraEksterntTopic(aktørId: String, fnr: String) = """
-        {
-          "aktørId" : "$aktørId",
-          "fnr" : "$fnr",
+        val lagreAssert: (String?, String) -> Unit = { aktørId, fødselsnummer ->
+            assertThat(aktørId).isEqualTo(testAktørId)
+            assertThat(fødselsnummer).isEqualTo(testFnr)
+            lagreFunksjonHarBlittKalt = true
         }
-    """.trimIndent()
+
+        val consumer = mockConsumer()
+        val pdlytter = PdlLytter(consumer,lagreAssert)
+        val rapid = TestRapid()
+        val melding = aktor(testAktørId, testFnr)
+
+        mottaAktorMelding(consumer, melding)
+        pdlytter.onReady(rapid)
+
+        Thread.sleep(300)
+        assertThat(lagreFunksjonHarBlittKalt).isTrue()
+    }
+
+    private fun aktor(aktørId: String, fødselsnummer: String) = Aktor(listOf(
+        Identifikator(aktørId, Type.AKTORID, true),
+        Identifikator(fødselsnummer, Type.FOLKEREGISTERIDENT, true)
+    ))
 
     private fun lagre(aktørId: String?, fnr: String) {
         testDatabase.lagreIdentMapping(
@@ -53,5 +62,27 @@ class PdllytterTest {
                 cachetTidspunkt = LocalDateTime.now()
             )
         )
+    }
+}
+private fun mottaAktorMelding(consumer: MockConsumer<String, Aktor>, aktor: Aktor, offset: Long = 0) {
+    val record = ConsumerRecord(
+        topic.topic(),
+        topic.partition(),
+        offset,
+        aktor.getIdentifikatorer().filter { it.getType()==Type.AKTORID }.map { it.getIdnummer() }.first(),
+        aktor,
+    )
+
+    consumer.schedulePollTask {
+        consumer.addRecord(record)
+    }
+}
+
+private val topic = TopicPartition(PdlLytterConfiguration.topicName, 0)
+
+private fun mockConsumer() = MockConsumer<String, Aktor>(OffsetResetStrategy.EARLIEST).apply {
+    schedulePollTask {
+        rebalance(listOf(topic))
+        updateBeginningOffsets(mapOf(Pair(topic, 0)))
     }
 }
