d57f232eb8be2ce7d0b7761db2d7471bc0df42b7
frodank <11638682+frodank@users.noreply.github.com>
2022-02-24 10:58:59 +0100

    Endrer tilretteleggingsbehov til å lese fra aiven-topic

--- apps/toi-tilretteleggingsbehov/src/test/kotlin/no/nav/arbeidsgiver/toi/KandidatEndretLytterTest.kt
+++ apps/toi-tilretteleggingsbehov/src/test/kotlin/no/nav/arbeidsgiver/toi/KandidatEndretLytterTest.kt
@@ -1,35 +1,24 @@
 package no.nav.arbeidsgiver.toi
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.clients.consumer.MockConsumer
-import org.apache.kafka.clients.consumer.OffsetResetStrategy
-import org.apache.kafka.common.TopicPartition
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-private val topicName = "aapen-tag-kandidatEndret-v1-default"
 
 class KandidatEndretLytterTest {
 
     @Test
     fun  `Lesing av melding på Kandidat Endret-topic skal føre til at en tilretteleggingsbehov-endret-melding blir publisert på rapid`() {
-        val consumer = mockConsumer()
-        val kandidatEndretLytter = KandidatEndretLytter(topicName, consumer)
         val rapid = TestRapid()
+        KandidatEndretLytter(rapid)
         val aktoerId = "123"
         val harTilretteleggingsbehov = true
         val behov = listOf("behov1", "behov2")
 
         val melding = melding(aktoerId, harTilretteleggingsbehov, behov)
 
-        mottaKandidatEndretMelding(consumer, aktoerId, melding)
+        rapid.sendTestMessage(melding)
 
-        kandidatEndretLytter.onReady(rapid)
-
-        Thread.sleep(300)
         val inspektør = rapid.inspektør
         assertThat(inspektør.size).isEqualTo(1)
 
@@ -51,29 +40,6 @@ class KandidatEndretLytterTest {
     }
 }
 
-private fun mockConsumer() = MockConsumer<String, String>(OffsetResetStrategy.EARLIEST).apply {
-    schedulePollTask {
-        rebalance(listOf(topic))
-        updateBeginningOffsets(mapOf(Pair(topic, 0)))
-    }
-}
-
-private fun mottaKandidatEndretMelding(consumer: MockConsumer<String, String>, key: String, melding: String, offset: Long = 0) {
-    val record = ConsumerRecord(
-        topic.topic(),
-        topic.partition(),
-        offset,
-        key,
-        melding,
-    )
-
-    consumer.schedulePollTask {
-        consumer.addRecord(record)
-    }
-}
-
-private val topic = TopicPartition(topicName, 0)
-
 private fun melding(aktoerId: String, harTilretteleggingsbehov: Boolean, behov: List<String> = emptyList()) = """
     {
         "aktoerId":"$aktoerId",
@@ -81,6 +47,3 @@ private fun melding(aktoerId: String, harTilretteleggingsbehov: Boolean, behov: 
         "behov":${behov.joinToString(prefix = "[", postfix = "]") {""""$it""""}}
     }
 """.trimIndent()
-
-private val objectMapper = ObjectMapper()
-    .registerModule(JavaTimeModule())
