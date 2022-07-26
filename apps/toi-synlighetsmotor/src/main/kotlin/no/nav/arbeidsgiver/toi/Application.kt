6ea985bf0641b07f7036fd229cb90309a7290b87
Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
2022-07-26 15:59:02 +0200

    Fixfeiliisalive (#30)
    * Logger pga feil med isalive
    
    * isalive ble ikke kalt fordi man spurte etter javalins isalive på feil port
    
    * noe ekstra logging
    
    * Enda mer simpelt for å nå isalive-endepunkt
    
    * Enda merkelig feil der man ikke når isalive-path
    
    * Mistenker isalive-kall stopper i tilgangskontroll
    
    * Fix for isalive-endepunkt

--- apps/toi-synlighetsmotor/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
+++ apps/toi-synlighetsmotor/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
@@ -3,6 +3,7 @@ package no.nav.arbeidsgiver.toi
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.ApiBuilder.post
+import io.javalin.http.Context
 import no.nav.arbeidsgiver.toi.rest.evaluerKandidatFraContext
 import no.nav.arbeidsgiver.toi.rest.hentSynlighetForKandidater
 import no.nav.helse.rapids_rivers.RapidApplication
@@ -18,9 +19,7 @@ fun startApp(
     rapidIsAlive: () -> Boolean
 ) {
     javalin.routes {
-        get("/javalin/isalive") { context ->
-            context.status(if (rapidIsAlive()) 200 else 500)
-        }
+        get("/isalive", isAlive(rapidIsAlive), Rolle.UNPROTECTED)
         get("/evaluering/{fnr}", evaluerKandidatFraContext(repository::hentMedFnr), Rolle.VEILEDER)
         post("/synlighet", hentSynlighetForKandidater(repository::hentEvalueringer), Rolle.ARBEIDSGIVER)
     }
@@ -28,6 +27,12 @@ fun startApp(
     rapidsConnection.also {
         SynlighetsLytter(it, repository)
     }.start()
+}
+
+private val isAlive: (() -> Boolean) -> (Context) -> Unit = { isAlive ->
+    { context ->
+        context.status(if (isAlive()) 200 else 500)
+    }
 }
 
 fun opprettJavalinMedTilgangskontroll(
