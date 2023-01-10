565733612f8f812ac31851baffe36a078cdecd54
Joar Aurdal <joar.aurdal@nav.no>
2023-01-10 14:20:03 +0100

    Legg til primærnøkkel for fritatt database

--- apps/toi-fritatt-kandidatsok/src/main/resources/V1__init.sql
+++ apps/toi-fritatt-kandidatsok/src/main/resources/V1__init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE fritattkandidatsok (
-    fodselsnummer VARCHAR(11),
+    id bigserial primary key,
+    fodselsnummer VARCHAR(11) UNIQUE,
     fritatt_kandidatsok BOOLEAN NOT NULL DEFAULT FALSE,
     sist_endret_av_veileder TEXT,
     sist_endret_av_system TEXT,
