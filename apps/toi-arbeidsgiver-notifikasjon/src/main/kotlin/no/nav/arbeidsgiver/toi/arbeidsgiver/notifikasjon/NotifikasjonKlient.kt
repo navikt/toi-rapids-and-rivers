1bc11e597a7d89c53d3ca579599bbae30f7e720c
Joar Aurdal <joar.aurdal@nav.no>
2023-01-17 15:59:16 +0100

    Legg til kall mot notifikasjonssystemet

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/NotifikasjonKlient.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/NotifikasjonKlient.kt
@@ -1,9 +1,12 @@
 package no.nav.arbeidsgiver.toi.presentertekandidater.notifikasjoner
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.kittinunf.fuel.Fuel
+import no.nav.arbeidsgiver.toi.arbeidsgiver.notifikasjon.log
 import java.time.LocalDateTime
 import java.util.*
 
-class NotifikasjonKlient {
+class NotifikasjonKlient(val url: String) {
 
     fun sendNotifikasjon(
         mottakerEpost: String,
@@ -25,8 +28,23 @@ class NotifikasjonKlient {
                 tidspunkt = LocalDateTime.now(),
                 mottakerEpost = mottakerEpost
             )
+        val (_, response, result) = Fuel.post(path = url).body(spørring).responseString()
 
-        // TODO: Send notifikasjon
+        if(response.statusCode != 200) {
+            log.error("Feilkode fra notifikasjonssystemet: ${response.statusCode}")
+            throw RuntimeException("Feilkode fra notifikasjonssystemet: ${response.statusCode} ${result.get()}")
+        }
+
+
+        val json = jacksonObjectMapper().readTree(result.get())
+
+        val errors = json["errors"]
+
+        if(!errors.isNull && errors.size() > 0) {
+            log.error("Feil fra notifiksjonssystemet ${errors.asText()}")
+            throw RuntimeException("Feil fra notifiksjonssystemet ${errors.asText()}")
+
+        }
 
     }
 }
