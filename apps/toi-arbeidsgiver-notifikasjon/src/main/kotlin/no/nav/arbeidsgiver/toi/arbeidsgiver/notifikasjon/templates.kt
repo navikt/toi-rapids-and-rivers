07348de583143410b80c70a93bdc6ccabe1cbc2a
Mads Lee Giil <mads.lee.giil@nav.no>
2023-01-17 10:59:25 +0100

    Lim inn fungerende spørring fra insomnia

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
@@ -59,6 +59,21 @@ fun lagGraphQlMutation(
             feilmelding
         }
       }
+    }    
+""".trimIndent()
+
+val fungerendeIInsomnia = """
+    {
+      "query": "mutation OpprettNyBeskjed(${pesostegn}eksternId: String! ${pesostegn}grupperingsId: String! ${pesostegn}merkelapp: String! ${pesostegn}virksomhetsnummer: String! ${pesostegn}tittel: String! ${pesostegn}lenke: String! ${pesostegn}tidspunkt: ISO8601DateTime! ${pesostegn}hardDeleteDuration: ISO8601Duration!) { nyBeskjed( nyBeskjed: { metadata: { virksomhetsnummer: ${pesostegn}virksomhetsnummer, eksternId: ${pesostegn}eksternId opprettetTidspunkt: ${pesostegn}tidspunkt grupperingsid: ${pesostegn}grupperingsId hardDelete: { om: ${pesostegn}hardDeleteDuration } } mottaker: { altinn: { serviceEdition: \"1\" serviceCode: \"5078\" } } notifikasjon: { merkelapp: ${pesostegn}merkelapp tekst: \"dette er tekst\" lenke: ${pesostegn}lenke } eksterneVarsler: { epost: { epostTittel: ${pesostegn}tittel epostHtmlBody: \"htmlbody\" mottaker: { kontaktinfo: { epostadresse: \"hei@hei.no\" } } sendetidspunkt: { tidspunkt: \"2001-12-24T10:44:01\"} } } } ) { __typename ... on NyBeskjedVellykket { id } ... on Error { feilmelding } }}",
+    	"variables": { 
+    		"eksternId": "ID", 
+    		"grupperingsId": "id", 
+    		"merkelapp": "merkelapp",
+    		"virksomhetsnummer": "virksomhetsnummer",
+    		"tittel": "tittel",
+    		"lenke": "lenke",
+    		"tidspunkt": "2001-12-24T10:44:01",
+    		"hardDeleteDuration": "P2DT3H4M"
+    	}
     }
-    
 """.trimIndent()
