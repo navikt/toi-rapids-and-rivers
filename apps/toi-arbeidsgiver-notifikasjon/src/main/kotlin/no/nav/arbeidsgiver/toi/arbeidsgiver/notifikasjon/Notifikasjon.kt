db747e3c0ef24166aec25aee640df060a04e8091
Joar Aurdal <joar.aurdal@nav.no>
2023-01-17 15:11:18 +0100

    Opprett en graphql spørring i lytter

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/Notifikasjon.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/Notifikasjon.kt
@@ -2,13 +2,33 @@ package no.nav.arbeidsgiver.toi.presentertekandidater.notifikasjoner
 
 import no.nav.arbeidsgiver.toi.arbeidsgiver.notifikasjon.TokendingsKlient
 import java.time.Duration
+import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import java.util.*
 
 class Notifikasjon(private val tokendingsKlient: TokendingsKlient) {
     private val skalSletteNotifikasjonOm = Duration.of(3, ChronoUnit.MONTHS)
 
-    fun sendNotifikasjon(accessToken: String, epostArbeidsgiver: String, stillingsId: UUID, virksomhetsnummer: String) {
+    fun sendNotifikasjon(
+        epostArbeidsgiver: String,
+        stillingsId: UUID,
+        virksomhetsnummer: String,
+        avsender: String,
+    ) {
+        val epostBody = lagEpostBody(
+            tittel = "Todo tittel",
+            tekst = "Todo tekst",
+            avsender = avsender
+        )
+
+        val spørring =
+            graphQlSpørringForCvDeltMedArbeidsgiver(
+                stillingsId = stillingsId.toString(),
+                virksomhetsnummer = virksomhetsnummer,
+                epostBody = epostBody,
+                tidspunkt = LocalDateTime.now(),
+                epostMottaker = epostArbeidsgiver
+            )
 
     }
 
--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/NotifikasjonLytter.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/NotifikasjonLytter.kt
@@ -1,9 +1,12 @@
 package no.nav.arbeidsgiver.toi.arbeidsgiver.notifikasjon
 
+import no.nav.arbeidsgiver.toi.presentertekandidater.notifikasjoner.graphQlSpørringForCvDeltMedArbeidsgiver
+import no.nav.arbeidsgiver.toi.presentertekandidater.notifikasjoner.lagEpostBody
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import java.time.LocalDateTime
 
 class NotifikasjonLytter(rapidsConnection: RapidsConnection) : River.PacketListener {
 
@@ -15,6 +18,24 @@ class NotifikasjonLytter(rapidsConnection: RapidsConnection) : River.PacketListe
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        TODO("Not yet implemented")
+        val stillingId = "";
+        val virksomhetsnummer = "";
+        val epostBody = lagEpostBody(
+            tittel = "",
+            tekst = "",
+            avsender = ""
+        )
+        val epostMottaker = "";
+
+        val spørring =
+            graphQlSpørringForCvDeltMedArbeidsgiver(
+                stillingsId = stillingId,
+                virksomhetsnummer = virksomhetsnummer,
+                epostBody = epostBody,
+                tidspunkt = LocalDateTime.now(),
+                epostMottaker = epostMottaker
+            )
+
+
     }
 }
