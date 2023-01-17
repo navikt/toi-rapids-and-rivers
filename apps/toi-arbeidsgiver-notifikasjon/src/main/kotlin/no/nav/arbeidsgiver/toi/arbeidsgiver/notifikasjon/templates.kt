9110591045941fe070619b54022e76c3f4149a8f
Mads Lee Giil <mads.lee.giil@nav.no>
2023-01-17 11:15:49 +0100

    Formater fungerende JSON-spørring

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
@@ -4,76 +4,83 @@ private val pesostegn = "$"
 
 fun lagEpostBody() = """ epost """
 
-fun lagGraphQlMutation(
-    epostArbeidsgiver: String,
-    stillingsId: String,
-    stillingstittel: String,
-    virksomhetsnummer: String,
-) = """
-    mutation OpprettNyBeskjed(
-      ${pesostegn}grupperingsid: String!
-      ${pesostegn}merkelapp: String!
-      ${pesostegn}virksomhetsnummer: String!
-      ${pesostegn}tittel: String!
-      ${pesostegn}lenke: String!
-      ${pesostegn}tidspunkt: ISO8601DateTime
-      ${pesostegn}hardDeleteDuration: ISO8601Duration
-    ) {
-      nyBeskjed(
-        nyBeskjed: {
-          metadata: {
-            virksomhetsnummer: ${pesostegn}virksomhetsnummer,
-            eksternId: "sdsds"
-            opprettetTidspunkt: ${pesostegn}tidspunkt
-            grupperingsid: ${pesostegn}grupperingsid
-             hardDelete: {
-                om: ${pesostegn}hardDeleteDuration
-            }
-          }
-          mottaker: {
-            altinn: {
-              serviceEdition: "1" 
-              serviceCode: "5078"
+// TODO: Må fjerne newlines
+fun lagGraphQlSpørring() = """
+    {
+        "query": "mutation OpprettNyBeskjed(
+            ${pesostegn}eksternId: String! 
+            ${pesostegn}grupperingsId: String! 
+            ${pesostegn}merkelapp: String! 
+            ${pesostegn}virksomhetsnummer: String! 
+            ${pesostegn}epostTittel: String! 
+            ${pesostegn}epostBody: String! 
+            ${pesostegn}epostMottaker: String! 
+            ${pesostegn}lenke: String! 
+            ${pesostegn}tidspunkt: ISO8601DateTime! 
+            ${pesostegn}hardDeleteDuration: ISO8601Duration!
+            ${pesostegn}notifikasjonTekst: String!
+            ${pesostegn}epostSendetidspunkt: ISO8601LocalDateTime
+            ) { 
+            nyBeskjed (
+                nyBeskjed: { 
+                    metadata: { 
+                        virksomhetsnummer: ${pesostegn}virksomhetsnummer
+                        eksternId: ${pesostegn}eksternId
+                        opprettetTidspunkt: ${pesostegn}tidspunkt
+                        grupperingsid: ${pesostegn}grupperingsId
+                        hardDelete: { 
+                            om: ${pesostegn}hardDeleteDuration 
+                        } 
+                    } 
+                    mottaker: { 
+                        altinn: { 
+                            serviceEdition: \"1\" 
+                            serviceCode: \"5078\" 
+                        } 
+                    } 
+                    notifikasjon: { 
+                        merkelapp: ${pesostegn}merkelapp 
+                        tekst:  ${pesostegn}notifikasjonTekst 
+                        lenke: ${pesostegn}lenke 
+                    } 
+                    eksterneVarsler: { 
+                        epost: { 
+                            epostTittel: ${pesostegn}epostTittel 
+                            epostHtmlBody: ${pesostegn}epostBody 
+                            mottaker: { 
+                                kontaktinfo: { 
+                                    epostadresse: ${pesostegn}epostMottaker 
+                                } 
+                            } 
+                            epostSendetidspunkt: { 
+                                tidspunkt: ${pesostegn}epostSendetidspunkt
+                            } 
+                        } 
+                    } 
+                } 
+            ) { 
+            __typename 
+            ... on NyBeskjedVellykket { 
+                id 
+            } 
+            ... on Error { 
+                feilmelding 
             } 
           }
-          notifikasjon: {
-            merkelapp: ${pesostegn}merkelapp
-            tekst: "dette er tekst"
-            lenke: ${pesostegn}lenke
-          }
-          eksterneVarsler: {
-            epost: {
-              epostTittel: ${pesostegn}tittel
-              epostHtmlBody: "htmlbody"
-              mottaker: { kontaktinfo: { epostadresse: "hei@hei.no" } }
-              sendetidspunkt: { tidspunkt: "2001-12-24T10:44:01"}
-            }
-          }
+        }",
+        "variables": { 
+            "eksternId": "ID", 
+            "grupperingsId": "id", 
+            "merkelapp": "merkelapp",
+            "virksomhetsnummer": "virksomhetsnummer",
+            "epostTittel": "tittel",
+            "epostBody": "body",
+            "epostMottaker": "hei@hei.no",
+            "lenke": "lenke",
+            "tidspunkt": "2001-12-24T10:44:01",
+            "hardDeleteDuration": "P2DT3H4M",
+            "notifikasjonTekst": "tekst",
+            "epostSendetidspunkt": "2001-12-24T10:44:01"
         }
-      ) {
-        __typename
-        ... on NyBeskjedVellykket {
-            id
-        }
-        ... on Error {
-            feilmelding
-        }
-      }
-    }    
-""".trimIndent()
-
-val fungerendeIInsomnia = """
-    {
-      "query": "mutation OpprettNyBeskjed(${pesostegn}eksternId: String! ${pesostegn}grupperingsId: String! ${pesostegn}merkelapp: String! ${pesostegn}virksomhetsnummer: String! ${pesostegn}tittel: String! ${pesostegn}lenke: String! ${pesostegn}tidspunkt: ISO8601DateTime! ${pesostegn}hardDeleteDuration: ISO8601Duration!) { nyBeskjed( nyBeskjed: { metadata: { virksomhetsnummer: ${pesostegn}virksomhetsnummer, eksternId: ${pesostegn}eksternId opprettetTidspunkt: ${pesostegn}tidspunkt grupperingsid: ${pesostegn}grupperingsId hardDelete: { om: ${pesostegn}hardDeleteDuration } } mottaker: { altinn: { serviceEdition: \"1\" serviceCode: \"5078\" } } notifikasjon: { merkelapp: ${pesostegn}merkelapp tekst: \"dette er tekst\" lenke: ${pesostegn}lenke } eksterneVarsler: { epost: { epostTittel: ${pesostegn}tittel epostHtmlBody: \"htmlbody\" mottaker: { kontaktinfo: { epostadresse: \"hei@hei.no\" } } sendetidspunkt: { tidspunkt: \"2001-12-24T10:44:01\"} } } } ) { __typename ... on NyBeskjedVellykket { id } ... on Error { feilmelding } }}",
-    	"variables": { 
-    		"eksternId": "ID", 
-    		"grupperingsId": "id", 
-    		"merkelapp": "merkelapp",
-    		"virksomhetsnummer": "virksomhetsnummer",
-    		"tittel": "tittel",
-    		"lenke": "lenke",
-    		"tidspunkt": "2001-12-24T10:44:01",
-    		"hardDeleteDuration": "P2DT3H4M"
-    	}
     }
 """.trimIndent()
