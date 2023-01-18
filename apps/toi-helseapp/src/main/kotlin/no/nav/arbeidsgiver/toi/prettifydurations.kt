b3722030ff1152acf58f0c822c0d0311bcb5142f
frodank <11638682+frodank@users.noreply.github.com>
2023-01-18 18:05:19 +0100

    Fikse feil med sekunder og fornorske duration-prettify

--- apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/prettifydurations.kt
+++ apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/prettifydurations.kt
@@ -2,12 +2,12 @@ package no.nav.arbeidsgiver.toi
 
 import java.time.Duration
 
-fun Duration.prettify() = days + hours + minutes + seconds
-private val Duration.days: String
-    get() = if(toDays()>0) "${toDays()} days " else ""
-private val Duration.hours: String
-    get() = if(days.isNotEmpty()||toHoursPart()>0) "${toHoursPart()} hours " else ""
-private val Duration.minutes: String
-    get() = if(hours.isNotEmpty()||toMinutesPart()>0) "${toMinutesPart()} minutes " else ""
-private val Duration.seconds: String
-    get() = if(minutes.isNotEmpty()||toSecondsPart()>0) "${toSecondsPart()} seconds" else "0"
+fun Duration.prettify() = dager + timer + minutter + sekunder
+private val Duration.dager: String
+    get() = if(toDays()>0) "${toDays()} dager " else ""
+private val Duration.timer: String
+    get() = if(dager.isNotEmpty()||toHoursPart()>0) "${toHoursPart()} timer " else ""
+private val Duration.minutter: String
+    get() = if(timer.isNotEmpty()||toMinutesPart()>0) "${toMinutesPart()} minutter " else ""
+private val Duration.sekunder: String
+    get() = if(minutter.isNotEmpty()||toSecondsPart()>0) "${toSecondsPart()} sekunder" else "0"
