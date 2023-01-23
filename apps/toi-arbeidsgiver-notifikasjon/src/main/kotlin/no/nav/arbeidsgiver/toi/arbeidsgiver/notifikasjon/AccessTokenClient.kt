55048132abc71630c5ac52495a193448d66f9ef1
Mads Lee Giil <mads.lee.giil@nav.no>
2023-01-23 12:31:18 +0100

    Sørg for at feilmelding kastes hvis miljøvariabel ikke er satt

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/AccessTokenClient.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/AccessTokenClient.kt
@@ -24,13 +24,13 @@ class AccessTokenClient(private val env: Map<String, String>) {
 
     private fun fetchAccessToken(): AccessTokenResponse {
         log.info("Prøver å hente nytt access token ...")
-        val url = env["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"] ?: throw RuntimeException("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT ikke satt")
+        val url = env.variable("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")
 
         val formData = listOf(
             "grant_type" to "client_credentials",
-            "client_secret" to env["AZURE_APP_CLIENT_SECRET"],
-            "client_id" to env["AZURE_APP_CLIENT_ID"],
-            "scope" to env["NOTIFIKASJON_API_SCOPE"]
+            "client_secret" to env.variable("AZURE_APP_CLIENT_SECRET"),
+            "client_id" to env.variable("AZURE_APP_CLIENT_ID"),
+            "scope" to env.variable("NOTIFIKASJON_API_SCOPE")
         )
 
         val (_, _, result) = FuelManager()
@@ -52,3 +52,5 @@ data class AccessTokenResponse(
     val access_token: String,
     val expires_in: Int
 )
+
+fun Map<String, String>.variable(felt: String) = this[felt] ?: error("$felt er ikke angitt")
