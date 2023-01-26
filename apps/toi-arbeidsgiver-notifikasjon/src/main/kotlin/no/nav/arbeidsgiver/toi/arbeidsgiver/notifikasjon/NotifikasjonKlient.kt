dd25552b70369978cbc9c6bde0faf4b586b945d8
Joar Aurdal <joar.aurdal@nav.no>
2023-01-26 15:48:07 +0100

    ta bort graphql logging

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/NotifikasjonKlient.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/NotifikasjonKlient.kt
@@ -41,10 +41,6 @@ class NotifikasjonKlient(
                 mottakerEpostAdresser = mottakerEpostadresser,
             )
 
-        println("")
-        println(spørring)
-        println("")
-
         val (_, response, result) = Fuel
             .post(path = url)
             .header("Content-type", "application/json")
