887cf258ead46d5f031d3b47010368f49b3bba92
frodank <11638682+frodank@users.noreply.github.com>
2023-04-04 12:39:56 +0200

    Fikse data-grunnlag etter feil på fredag (oppfølgingsperiode)

--- apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
+++ apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/Application.kt
@@ -67,7 +67,7 @@ suspend fun sjekkOffsets(envs: Map<String, String>) {
         "toi-kandidatfeed" to "toi-kandidatfeed-rapidconsumer-6",
         "toi-maa-behandle-tidligere-cv" to "toi-maa-behandle-tidligere-cv-rapidconsumer-4",
         "toi-oppfolgingsinformasjon" to "toi-oppfølgingsinformasjon-rapidconsumer-4",
-        "toi-oppfolgingsperiode" to "toi-oppfølgingsperiode-rapidconsumer-1",
+        "toi-oppfolgingsperiode" to "toi-oppfølgingsperiode-rapidconsumer-2",
         "toi-organisasjonsenhet" to "toi-organisasjonsenhet-rapidconsumer-1",
         "toi-sammenstille-kandidat" to "toi-sammenstille-kandidat-rapidconsumer-1",
         "toi-siste-14a-vedtak" to "toi-siste-14a-vedtak-rapidconsumer-2",
