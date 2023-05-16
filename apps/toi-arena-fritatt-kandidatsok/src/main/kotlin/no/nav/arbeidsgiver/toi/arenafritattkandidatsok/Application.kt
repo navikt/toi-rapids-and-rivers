c93e88c907fe475241f4dc9c78281d08119a0151
Joar Aurdal <joar.aurdal@nav.no>
2023-05-16 14:26:54 +0200

    Bytt scheduler tidspunkt

--- apps/toi-arena-fritatt-kandidatsok/src/main/kotlin/no/nav/arbeidsgiver/toi/arenafritattkandidatsok/Application.kt
+++ apps/toi-arena-fritatt-kandidatsok/src/main/kotlin/no/nav/arbeidsgiver/toi/arenafritattkandidatsok/Application.kt
@@ -17,7 +17,7 @@ fun main() {
         register(object : RapidsConnection.StatusListener {
             override fun onStartup(rapidsConnection: RapidsConnection) {
                 repository.flywayMigrate(dataSource)
-                startFritattScedulerKlokken(13,50,0,0, repository, this@apply)
+                startFritattScedulerKlokken(14,35,0,0, repository, this@apply)
             }
         })
 
