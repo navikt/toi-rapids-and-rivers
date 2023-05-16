15a162d25612ec68ee28195b1e8e4c0bf05a4458
Joar Aurdal <joar.aurdal@nav.no>
2023-05-16 14:56:55 +0200

    Kjør en ekstra runde i dev

--- apps/toi-arena-fritatt-kandidatsok/src/main/kotlin/no/nav/arbeidsgiver/toi/arenafritattkandidatsok/Application.kt
+++ apps/toi-arena-fritatt-kandidatsok/src/main/kotlin/no/nav/arbeidsgiver/toi/arenafritattkandidatsok/Application.kt
@@ -17,7 +17,7 @@ fun main() {
         register(object : RapidsConnection.StatusListener {
             override fun onStartup(rapidsConnection: RapidsConnection) {
                 repository.flywayMigrate(dataSource)
-                startFritattScedulerKlokken(14,35,0,0, repository, this@apply)
+                startFritattScedulerKlokken(15,5,0,0, repository, this@apply)
             }
         })
 
