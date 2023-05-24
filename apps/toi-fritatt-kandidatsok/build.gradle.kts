2801fa8af4414a1b67f1dddeb56a759390224a41
Joar Aurdal <joar.aurdal@nav.no>
2023-05-24 16:16:37 +0200

    Fjern gammel fritatt kandidatsøk applikasjon

--- apps/toi-fritatt-kandidatsok/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("toi.rapids-and-rivers")
-}
-
-dependencies {
-    implementation("no.nav.arbeid.cv.avro:cv-event:29")
-    implementation("io.confluent:kafka-avro-serializer:7.2.2")
-    implementation("org.codehaus.jackson:jackson-mapper-asl:1.9.13")
-}
