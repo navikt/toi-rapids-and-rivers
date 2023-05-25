54e2cf0507a6f65210b8b43f6f0fd6a1fc5b8a6d
Joar Aurdal <joar.aurdal@nav.no>
2023-05-25 14:11:33 +0200

    Ta bort gammel fritatt fra gradle bygg

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,6 @@ include(
     "apps:toi-identmapper",
     "apps:toi-synlighetsmotor",
     "apps:toi-oppfolgingsperiode",
-    "apps:toi-fritatt-kandidatsok",
     "apps:toi-hjemmel",
     "apps:toi-maa-behandle-tidligere-cv",
     "apps:toi-organisasjonsenhet",
