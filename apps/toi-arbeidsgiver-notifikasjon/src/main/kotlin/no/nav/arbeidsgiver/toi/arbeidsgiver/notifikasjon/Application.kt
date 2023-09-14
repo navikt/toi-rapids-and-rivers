9744e0173e203607be739f8ef98e400c6e040430
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-09-14 12:33:24 +0200

    Test endepunkt for epost-mal

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/Application.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/Application.kt
@@ -6,25 +6,31 @@ import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.nio.charset.StandardCharsets
 
 fun startApp(
     notifikasjonKlient: NotifikasjonKlient,
     rapidsConnection: RapidsConnection,
     rapidIsAlive: () -> Boolean
 ) {
-    Javalin.create().routes {
-        get("/isalive") { context ->
-            context.status(if (rapidIsAlive()) 200 else 500)
-        }
-
-        get("/template") { context ->
-            context.html(epostTemplate).status(200)
-        }
-    }.start(8301)
-
+    opprettJavalin(rapidIsAlive)
     NotifikasjonLytter(rapidsConnection, notifikasjonKlient)
 
     rapidsConnection.start()
+}
+
+fun opprettJavalin(rapidIsAlive: () -> Boolean) {
+    Javalin.create().routes {
+        get("/isalive") { context ->
+            context
+                .status(if (rapidIsAlive()) 200 else 500)
+        }
+
+        get("/template") { context ->
+            context
+                .html(epostTemplate).status(200)
+        }
+    }.start(8301)
 }
 
 fun main() {
--- apps/toi-arbeidsgiver-notifikasjon/src/test/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/EpostTemplateTest.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/test/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/EpostTemplateTest.kt
@@ -1,4 +1,6 @@
 package no.nav.arbeidsgiver.toi.arbeidsgiver.notifikasjon
+import com.github.kittinunf.fuel.Fuel
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 
 import org.junit.jupiter.api.Test
@@ -6,6 +8,18 @@ import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EpostTemplateTest {
+
+
+    @Test
+    fun `GET-kall mot template returnerer en statisk HTML-mal for e-poster som brukes mot arbeidsgivere`() {
+        opprettJavalin { true }
+
+        val response = Fuel.get("http://localhost:8301/template")
+            .response().second
+
+        assertThat(response.statusCode).isEqualTo(200)
+        assertThat(response.body().asString("text/html;charset=UTF-8")).isEqualTo(epostTemplate)
+    }
 
     @Test
     fun `lagEpostBody erstatter plassholdere med tittel, tekst og avsender`() {
