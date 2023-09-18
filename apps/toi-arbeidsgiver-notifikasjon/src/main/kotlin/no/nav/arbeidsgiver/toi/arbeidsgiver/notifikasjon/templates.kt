f557ec96513c33cb850f2ed966de38a7692c39b2
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2023-09-18 15:34:21 +0200

    Åpne et endepunkt for å hente epost-mal (#63)
    * Isoler epost-malen til en string
    
    * Legg til newline på slutten av fil
    
    * Start på et endepunkt
    
    * Oppgrader til javalin@5 og wiremock@3
    
    * Test endepunkt for epost-mal
    
    * Gi skriverettigheter til toi-arbeidsgiver-notifikasjon
    
    * Deploy til dev
    
    * Oppdater nais-config med ingress, inbound-rule og porter
    
    * Gjør det enkelt å replace strenger i frontend
    
    * Oppdater tester

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
@@ -6,17 +6,21 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
-    fun lagEpostBody(tittel: String, tekst: String, avsender: String) = """
+const val TITTEL = "{tittel}"
+const val TEKST = "{tekst}"
+const val AVSENDER = "{avsender}"
+
+val epostTemplate = """
     <html>
         <head>
             <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
-            <title>${tittel.htmlEscape()}</title>
+            <title id='tittel'>$TITTEL</title>
         </head>
         <body style='font-family: sans-serif; padding:40px 20px; color: #262626'>
             <div>
                 <h1 style='font-size: 1.75rem; font-weight:bold;'>Hei.</h1>
-                <h2 style='font-weight: 500; font-size: 1rem;'>Vi har funnet nye kandidater for deg til stillingen: <b>${tittel.htmlEscape()}</b>.</h2>
-                <pre style='font-family: unset;'>${tekst.htmlEscape()}</pre>
+                <h2 style='font-weight: 500; font-size: 1rem;'>Vi har funnet nye kandidater for deg til stillingen: <b id='stillingstittel'>$TITTEL</b>.</h2>
+                <pre style='font-family: unset;' id='tekst'>$TEKST</pre>
             </div>
     
     
@@ -33,7 +37,7 @@ import java.time.temporal.ChronoUnit
                 </div>
             </div>
     
-            <p style='padding-block:40px 32px'>Vennlig hilsen $avsender</p>
+            <p style='padding-block:40px 32px'>Vennlig hilsen <span id='avsender'>$AVSENDER</span></p>
     
             <div style='border:1px solid rgba(0, 0, 0, 0.1)'></div>
     
@@ -74,6 +78,13 @@ import java.time.temporal.ChronoUnit
         </body>
     </html>
 """.trimIndent()
+
+fun lagEpostBody(tittel: String, tekst: String, avsender: String): String {
+    return epostTemplate
+        .replace(TITTEL, tittel.htmlEscape())
+        .replace(TEKST, tekst.htmlEscape())
+        .replace(AVSENDER, avsender)
+}
 
 fun graphQlSpørringForCvDeltMedArbeidsgiver(
     notifikasjonsId: String,
---
+++ apps/toi-arbeidsgiver-notifikasjon/src/test/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/EpostTemplateTest.kt
@@ -0,0 +1,33 @@
+package no.nav.arbeidsgiver.toi.arbeidsgiver.notifikasjon
+import com.github.kittinunf.fuel.Fuel
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class EpostTemplateTest {
+
+
+    @Test
+    fun `GET-kall mot template returnerer en statisk HTML-mal for e-poster som brukes mot arbeidsgivere`() {
+        opprettJavalin { true }
+
+        val response = Fuel.get("http://localhost:8301/template")
+            .response().second
+
+        assertThat(response.statusCode).isEqualTo(200)
+        assertThat(response.body().asString("text/html;charset=UTF-8")).isEqualTo(epostTemplate)
+    }
+
+    @Test
+    fun `lagEpostBody erstatter plassholdere med tittel, tekst og avsender`() {
+        val body = lagEpostBody("En slags jobb", "Hei, her kommer to nye kandidater!", "Vetle Veileder")
+
+        assertThat(body).contains("<title id='tittel'>En slags jobb</title>")
+        assertThat(body).contains("<b id='stillingstittel'>En slags jobb</b>")
+        assertThat(body).contains("<pre style='font-family: unset;' id='tekst'>Hei, her kommer to nye kandidater!</pre>")
+        assertThat(body).contains("<span id='avsender'>Vetle Veileder</span>")
+    }
+}
