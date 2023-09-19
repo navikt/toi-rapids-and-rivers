bd4ea47b29343af093d36b356d48ae7f81d9c7c2
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-09-19 08:57:52 +0200

    WIP: Fiks epostformatering

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/templates.kt
@@ -11,67 +11,61 @@ const val TEKST = "{tekst}"
 const val AVSENDER = "{avsender}"
 
 val epostTemplate = """
+    <!DOCTYPE html>
     <html>
         <head>
-            <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
-            <title id='tittel'>$TITTEL</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+            <title id="tittel">$TITTEL</title>
         </head>
-        <body style='font-family: sans-serif; padding:40px 20px; color: #262626'>
-            <div>
-                <h1 style='font-size: 1.75rem; font-weight:bold;'>Hei.</h1>
-                <h2 style='font-weight: 500; font-size: 1rem;'>Vi har funnet nye kandidater for deg til stillingen: <b id='stillingstittel'>$TITTEL</b>.</h2>
-                <pre style='font-family: unset;' id='tekst'>$TEKST</pre>
+        <body style="font-family: sans-serif; padding: 40px 20px; color: #23262A">
+            <h1 style="font-size: 32px; font-weight: bold;">Hei.</h1>
+            <p>Vi har funnet nye kandidater for deg til stillingen: <b id="stillingstittel">$TITTEL</b>.</p>
+            <pre style="font-family: sans-serif; margin-top: 32px; margin-bottom: 32px;" id="tekst">$TEKST</pre>
+
+            <h3 style="font-size: 16px;">For å se kandidatene dine</h3>
+            <div style="padding: 16px; margin-bottom: 24px; border-radius: 12px; background-color:#F2F3F5;">
+                <ol style="margin: 0px; padding-left: 24px;">
+                    <li style="margin-bottom: 8px;">Åpne <a href="#" style="text-decoration:none; color:#000000; cursor: default; font-weight: bold;">https://arbeidsgiver.nav.no/min-side-arbeidsgiver</a></li>
+                    <li style="margin-bottom: 8px;"><b>Logg inn</b></li>
+                    <li>Finn <b>varslene dine</b> oppe til høyre på siden, og trykk deg inn på meldingen, eller finn lenken i kortet med teksten <b>Kandidater til mine stillinger</b> lenger ned på siden.</li>
+                </ol>
             </div>
-    
-    
-            <div style='padding-block-start:32px;'>
-                <h3 style='font-size: 1rem; line-height: 1rem; font-weight:bold;'>Se kandidatene dine</h3>
-                <div style='font-size: 1rem; padding: 12px 16px; border-radius: 12px; background-color:#F7F7F7;'>
-                    <p>
-                    <ol style='margin-block: 0px; padding-inline-start: 24px;'>
-                        <li style='margin-block-end: 0.5rem;'>Åpne <a href='#' style='text-decoration:none; color:#000; cursor: default; font-weight: bold;'>https://arbeidsgiver.nav.no/min-side-arbeidsgiver</a></li>
-                        <li style='margin-block-end: 0.5rem;'><b>Logg inn</b></li>
-                        <li>Finn <b>varslene dine</b> oppe til høyre på siden, og trykk deg inn på meldingen, eller finn lenken i kortet med teksten <b>Kandidater til mine stillinger</b> lenger ned på siden.</li>
-                    </ol>
-                    </p>
-                </div>
-            </div>
-    
-            <p style='padding-block:40px 32px'>Vennlig hilsen <span id='avsender'>$AVSENDER</span></p>
-    
-            <div style='border:1px solid rgba(0, 0, 0, 0.1)'></div>
-    
-            <h2 style='font-size: 1rem; line-height: 1rem; font-weight:bold; margin-block: 56px 20px; color:rgba(0,0,0,0.56);'>Mangler du tilgang til Min Side for Arbeidsgiver hos NAV?</h2>
-    
-            <div style='font-size: 1rem; padding:24px 24px; border-radius: 12px; background-color:#F7F7F7;'>
-                <p style='padding-block: 0px; margin-block-end: 0.5rem; margin-block-start: 0px'>Tilgangen til NAVs rekrutteringstjenester styrer arbeidsgivere selv i <b>Altinn</b>.</p>
-                <p style='margin-block-end: 0.5rem;'>For å få tilgang må du kontakte den som styrer tilgangene til virksomheten din. Det kan være noen i HR, en leder, mellomleder, eller noen på eiersiden i virksomheten.</p>
-                <p style='margin-block-end: 0.5rem;'>Vi har lagd en oppskrift du kan dele med vedkommende for å gjøre det enklere for dem å gi deg tilgang.</p>
-    
-                <h3 style='font-size: 1rem;line-height: 1rem; font-weight:500; margin-block: 40px 32px'>Kopier den gjerne og send den til vedkommende:</h3>
-                <div style='font-size: 1rem; padding:24px 24px; border-radius: 12px; border: 3px dashed rgba(0, 0, 0, 0.1); background-color:#fff;'>
-                    <p style='padding-block: 0px; margin-block-end: 0.5rem; margin-block-start: 0px'>Du får denne meldingen fordi avsender ønsker å få tilgang til CV-er fra NAV på vegne av virksomheten din. </p>
+
+            <p>Vennlig hilsen <span id="avsender">$AVSENDER</span></p>
+
+            <div style="border: 1px solid #CBCFD5; margin-top: 32px; margin-bottom: 32px;" ></div>
+
+            <h2 style="font-size: 16px; margin-bottom: 24px;">Mangler du tilgang til Min Side for Arbeidsgiver hos NAV?</h2>
+
+            <div style="font-size: 16px; padding: 24px 24px; border-radius: 12px; background-color: #F2F3F5;">
+                <p style="padding: 0px; margin-bottom: 8px; margin-top: 0px">Tilgangen til NAVs rekrutteringstjenester styrer arbeidsgivere selv i <b>Altinn</b>.</p>
+                <p style="margin-bottom: 8px;">For å få tilgang må du kontakte den som styrer tilgangene til virksomheten din. Det kan være noen i HR, en leder, mellomleder, eller noen på eiersiden i virksomheten.</p>
+                <p style="margin-bottom: 8px;">Vi har lagd en oppskrift du kan dele med vedkommende for å gjøre det enklere for dem å gi deg tilgang.</p>
+
+                <h3 style="font-size: 16px; line-height: 16px; font-weight: 500; margin: 40px 32px">Kopier den gjerne og send den til vedkommende:</h3>
+                <div style="font-size: 16px; padding:24px 24px; border-radius: 12px; border: 3px dashed #CBCFD5; background-color: #FFFFFF;">
+                    <p style="padding: 0px; margin-bottom: 8px; margin-top: 0px">Du får denne meldingen fordi avsender ønsker å få tilgang til CV-er fra NAV på vegne av virksomheten din. </p>
                     <b>Gi tilganger til CV-er fra NAV:</b>
                     <ol>
                         <li>Logg inn i Altinn</li>
-                        <li>Velg virksomheten din under 'Alle dine aktører'</li>
-                        <li>Trykk på knappen 'Profil' øverst i menyen</li>
-                        <li>Trykk på 'Andre med rettigheter til virksomheten'</li>
-                        <li>Velg 'Legge til ny person eller virksomhet'</li>
+                        <li>Velg virksomheten din under «Alle dine aktører»</li>
+                        <li>Trykk på knappen «Profil» øverst i menyen</li>
+                        <li>Trykk på «Andre med rettigheter til virksomheten»</li>
+                        <li>Velg «Legge til ny person eller virksomhet»</li>
                         <li>Legg inn personnummeret og etternavnet til personen som skal ha tilgang</li>
-                        <li>Velg 'Gi tilgang til enkelttjenester'</li>
-                        <li>Skriv 'Rekruttering', så vil alternativet komme opp som et valg. Velg 'Rekruttering'.</li>
+                        <li>Velg «Gi tilgang til enkelttjenester»</li>
+                        <li>Skriv «Rekruttering», så vil alternativet komme opp som et valg. Velg «Rekruttering».</li>
                         <li>Bekreft</li>
                     </ol>
                     <p>
-                        Denne enkeltrettigheten gir kun tilgang til å bruke NAV sine rekrutteringstjenester: publisere stillingsannonser og holde videomøter for stillinger på Arbeidsplassen, og motta CV-er fra NAV på <a href='#' style='text-decoration:none; color:#000; cursor: default; font-weight: bold;'>https://arbeidsgiver.nav.no/min-side-arbeidsgiver</a>.
+                        Denne enkeltrettigheten gir kun tilgang til å bruke NAV sine rekrutteringstjenester: publisere stillingsannonser og holde videomøter for stillinger på Arbeidsplassen, og motta CV-er fra NAV på <a href="#" style="text-decoration:none; color:#000; cursor: default; font-weight: bold;">https://arbeidsgiver.nav.no/min-side-arbeidsgiver</a>.
                     </p>
                     <b>Ga ikke Altinn deg muligheten til å gi tilgang?</b>
                     <p>Du kan gi tilgang hvis du har en av disse rollene:</p>
                     <ul>
                         <li>Du er registrert i Enhetsregisteret som daglig leder, styrets leder, bestyrende reder eller innehaver.</li>
                         <li>Du er registert  som hovedadministrator i Altinn.</li>
-                        <li>Du er 'Tilgangsstyrer' i Altinn og har én, eller flere av rollene: 'Rekruttering', 'Lønn og personalmedarbeider', eller 'Utfyller/innsender'.</li>
+                        <li>Du er «Tilgangsstyrer» i Altinn og har én, eller flere av rollene: «Rekruttering», «Lønn og personalmedarbeider», eller «Utfyller/innsender».</li>
                     </ul>
                 </div>
             </div>
