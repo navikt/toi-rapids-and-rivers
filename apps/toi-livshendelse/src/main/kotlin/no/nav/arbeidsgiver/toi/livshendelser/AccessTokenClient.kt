80ce021530d115009761d8bdfda5774e1fa4d06c
Joar Aurdal <joar.aurdal@nav.no>
2023-10-05 16:07:21 +0200

    Sjekk at token paramatere har verdi

--- apps/toi-livshendelse/src/main/kotlin/no/nav/arbeidsgiver/toi/livshendelser/AccessTokenClient.kt
+++ apps/toi-livshendelse/src/main/kotlin/no/nav/arbeidsgiver/toi/livshendelser/AccessTokenClient.kt
@@ -6,14 +6,19 @@ import com.github.kittinunf.fuel.jackson.responseObject
 import java.time.Instant
 import kotlin.RuntimeException
 import com.github.kittinunf.result.Result
+import org.slf4j.LoggerFactory
 
 class AccessTokenClient(private val env: Map<String, String>) {
+    private val secureLog = LoggerFactory.getLogger("secureLog")
+
     private var tokenUtgår = Instant.ofEpochSecond(0)
     private var cachedToken = AccessTokenResponse("uinitialisert", 0)
         set(value) {
             field = value
             tokenUtgår = Instant.now().minusSeconds(30).plusSeconds(value.expires_in.toLong())
         }
+
+
 
     fun hentAccessToken(): String {
         if (Instant.now().isAfter(tokenUtgår))
@@ -23,7 +28,15 @@ class AccessTokenClient(private val env: Map<String, String>) {
     }
 
     private fun fetchAccessToken(): AccessTokenResponse {
-        log.info("Prøver å hente nytt access token ...")
+        secureLog.info("Prøver å hente nytt access token")
+
+        if(env["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"] == null ||
+            env["AZURE_APP_CLIENT_SECRET"] == null ||
+            env["AZURE_APP_CLIENT_ID"] == null ||
+            env["PDL_SCOPE"] == null) {
+            secureLog.info("Access token kall mangler data")
+        }
+
         val url = env["AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"] ?: throw RuntimeException("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT ikke satt")
 
         val formData = listOf(
@@ -38,12 +51,12 @@ class AccessTokenClient(private val env: Map<String, String>) {
             .responseObject<AccessTokenResponse>()
 
         when (result) {
-             is Result.Success -> {
-                 log.info("Fikk tak i access token med lengde ${result.get().access_token.length}")
-                 return result.get()
-             }
+            is Result.Success -> {
+                log.info("Fikk tak i access token med lengde ${result.get().access_token.length}")
+                return result.get()
+            }
 
-             is Result.Failure -> throw RuntimeException("Noe feil skjedde ved henting av access_token: ", result.getException())
+            is Result.Failure -> throw RuntimeException("Noe feil skjedde ved henting av access_token: ", result.getException())
         }
     }
 }
