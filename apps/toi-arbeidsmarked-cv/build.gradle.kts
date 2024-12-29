07c6d12452b90d8e48d1e1f7d4a953e0aca4689f
Are Husby <are.husby@nav.no>
2024-12-29 17:44:10 +0100

    Oppgrader avhengigheter i toi-arbeidsmarked-cv

--- apps/toi-arbeidsmarked-cv/build.gradle.kts
+++ apps/toi-arbeidsmarked-cv/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("toi.rapids-and-rivers")
-    id("com.github.davidmc24.gradle.plugin.avro") version "1.5.0"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
 }
 
 dependencies {
-    implementation("io.confluent:kafka-avro-serializer:7.2.2")
+    implementation("io.confluent:kafka-avro-serializer:7.8.0")
     implementation("org.codehaus.jackson:jackson-mapper-asl:1.9.13")
-    implementation("org.apache.avro:avro:1.11.1")
+    implementation("org.apache.avro:avro:1.12.0")
 }
