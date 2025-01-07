7927f6698b696bb782c0818e759359fcde629e7f
Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
2025-01-07 13:33:15 +0100

    Oppgraderer avhengigheter i dev for identmapper (#87)
    * Oppgraderer avhengigheter i dev for identmapper
    
    * Legg tol postgres flyway siden det trengs for nye flyway
    
    ---------
    
    Co-authored-by: Joar Aurdal <joaraurdal@joars-MacBook-Pro.local>

--- apps/toi-identmapper/build.gradle.kts
+++ apps/toi-identmapper/build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
     id("toi.rapids-and-rivers")
-    id("com.github.davidmc24.gradle.plugin.avro") version "1.5.0"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
 }
 
 dependencies {
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.github.kittinunf.fuel:fuel-jackson:2.3.1")
-    implementation("org.apache.avro:avro:1.11.1")
-    implementation("io.confluent:kafka-avro-serializer:7.2.2")
+    implementation("org.apache.avro:avro:1.12.0")
+    implementation("io.confluent:kafka-avro-serializer:7.8.0")
 
     // Database
-    implementation("org.postgresql:postgresql:42.5.1")
-    implementation("org.flywaydb:flyway-core:9.8.2")
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    testImplementation("com.h2database:h2:2.1.214")
+    implementation("org.postgresql:postgresql:42.7.4")
+    implementation("org.flywaydb:flyway-core:11.1.0")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:11.1.0")
+    implementation("com.zaxxer:HikariCP:6.2.1")
+    testImplementation("com.h2database:h2:2.3.232")
 }
