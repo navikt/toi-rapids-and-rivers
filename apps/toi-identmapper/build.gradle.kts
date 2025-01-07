311dd1d6294ab718954b6a5c59ba900f1aae0d29
Joar Aurdal <joaraurdal@joars-MacBook-Pro.local>
2025-01-07 13:03:45 +0100

    Legg tol postgres flyway siden det trengs for nye flyway

--- apps/toi-identmapper/build.gradle.kts
+++ apps/toi-identmapper/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     // Database
     implementation("org.postgresql:postgresql:42.7.4")
     implementation("org.flywaydb:flyway-core:11.1.0")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:11.1.0")
     implementation("com.zaxxer:HikariCP:6.2.1")
     testImplementation("com.h2database:h2:2.3.232")
 }
