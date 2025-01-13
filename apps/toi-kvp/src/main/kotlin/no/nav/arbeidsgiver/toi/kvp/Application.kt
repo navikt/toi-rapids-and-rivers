c1fb95114bde5a5d6351676ce0bddcf86941733e
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-01-13 16:07:32 +0100

    oppgrader rapids and riverws

--- apps/toi-kvp/src/main/kotlin/no/nav/arbeidsgiver/toi/kvp/Application.kt
+++ apps/toi-kvp/src/main/kotlin/no/nav/arbeidsgiver/toi/kvp/Application.kt
@@ -3,7 +3,7 @@ package no.nav.arbeidsgiver.toi.kvp
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import no.nav.helse.rapids_rivers.JsonMessage
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
