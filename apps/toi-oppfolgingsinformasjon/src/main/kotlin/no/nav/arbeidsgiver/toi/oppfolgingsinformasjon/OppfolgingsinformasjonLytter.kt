a3fa34fa47a6b30840fdbbc8cb588c28c00a053c
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-01-13 16:37:17 +0100

    Oppgrader rapids and rivers for oppfølgingsinformasjon

--- apps/toi-oppfolgingsinformasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/oppfolgingsinformasjon/OppfolgingsinformasjonLytter.kt
+++ apps/toi-oppfolgingsinformasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/oppfolgingsinformasjon/OppfolgingsinformasjonLytter.kt
@@ -3,25 +3,32 @@ package no.nav.arbeidsgiver.toi.oppfolgingsinformasjon
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import no.nav.helse.rapids_rivers.JsonMessage
-import no.nav.helse.rapids_rivers.MessageContext
-import no.nav.helse.rapids_rivers.RapidsConnection
-import no.nav.helse.rapids_rivers.River
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
+import com.github.navikt.tbd_libs.rapids_and_rivers.River
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
+import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
+import io.micrometer.core.instrument.MeterRegistry
 
 class OppfolgingsinformasjonLytter(private val rapidsConnection: RapidsConnection) : River.PacketListener {
     init {
         River(rapidsConnection).apply {
-            validate {
-                it.demandKey("fodselsnummer")
-                it.demandKey("oppfolgingsenhet")
-                it.demandKey("harOppfolgingssak")
+            precondition{
+                it.requireKey("fodselsnummer")
+                it.requireKey("oppfolgingsenhet")
+                it.requireKey("harOppfolgingssak")
+                it.forbid("@event_name")
                 it.interestedIn("sistEndretDato")
-                it.rejectKey("@event_name")
             }
         }.register(this)
     }
 
-    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+    override fun onPacket(
+        packet: JsonMessage,
+        context: MessageContext,
+        metadata: MessageMetadata,
+        meterRegistry: MeterRegistry
+    ) {
         val melding = mapOf(
             "fodselsnummer" to packet["fodselsnummer"],
             "oppfølgingsinformasjon" to packet.fjernMetadataOgKonverter(),
@@ -36,7 +43,8 @@ class OppfolgingsinformasjonLytter(private val rapidsConnection: RapidsConnectio
 
     private fun JsonMessage.fjernMetadataOgKonverter(): JsonNode {
         val jsonNode = jacksonObjectMapper().readTree(this.toJson()) as ObjectNode
-        val metadataFelter = listOf("system_read_count", "system_participating_services", "@event_name", "@id", "@opprettet")
+        val metadataFelter =
+            listOf("system_read_count", "system_participating_services", "@event_name", "@id", "@opprettet")
         jsonNode.remove(metadataFelter)
         return jsonNode
     }
