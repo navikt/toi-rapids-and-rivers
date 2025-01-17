43c4a109d4d8f2920b303cc9708567585d902822
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-01-17 15:29:48 +0100

    Oppgrader avhengigheter (#93)

--- apps/toi-livshendelse/build.gradle.kts
+++ apps/toi-livshendelse/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("toi.rapids-and-rivers")
-    id("com.github.davidmc24.gradle.plugin.avro") version "1.5.0"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
 }
 
 dependencies {
-    implementation("org.apache.avro:avro:1.11.1")
-    implementation("io.confluent:kafka-avro-serializer:7.2.2")
+    implementation("org.apache.avro:avro:1.12.0")
+    implementation("io.confluent:kafka-avro-serializer:7.8.0")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.github.kittinunf.fuel:fuel-jackson:2.3.1")
 
-    testImplementation("no.nav.security:mock-oauth2-server:0.5.6")
-    testImplementation("org.wiremock:wiremock-standalone:3.0.4")
+    testImplementation("no.nav.security:mock-oauth2-server:2.1.0")
+    testImplementation("org.wiremock:wiremock-standalone:3.10.0")
 }
