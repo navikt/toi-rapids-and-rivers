2afabef3d180b2c4690028a9c68506aa1fa6851b
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-01-23 14:29:58 +0100

    Fjern securelogging i synlighetsmotor

--- apps/toi-synlighetsmotor/src/main/kotlin/no/nav/arbeidsgiver/toi/SynlighetsLytter.kt
+++ apps/toi-synlighetsmotor/src/main/kotlin/no/nav/arbeidsgiver/toi/SynlighetsLytter.kt
@@ -47,7 +47,6 @@ class SynlighetsLytter(private val rapidsConnection: RapidsConnection, private v
         val kandidat = Kandidat.fraJson(packet)
 
         val synlighetsevaluering = lagEvalueringsGrunnlag(kandidat)
-        securelogAdressebeskyttelse(kandidat, synlighetsevaluering)
         repository.lagre(
             evaluering = synlighetsevaluering,
             aktørId = kandidat.aktørId,
@@ -57,72 +56,5 @@ class SynlighetsLytter(private val rapidsConnection: RapidsConnection, private v
         val synlighet = synlighetsevaluering()
         packet["synlighet"] = synlighet
         rapidsConnection.publish(kandidat.aktørId, packet.toJson())
-    }
-
-    fun securelogAdressebeskyttelse(kandidat: Kandidat, synlighetsevaluering: Evaluering) {
-        val synligUtenomGradering = synlighetsevaluering.run {
-            harAktivCv &&
-                    harJobbprofil &&
-                    harSettHjemmel &&
-                    maaIkkeBehandleTidligereCv &&
-                    arenaIkkeFritattKandidatsøk &&
-                    erUnderOppfoelging &&
-                    harRiktigFormidlingsgruppe &&
-                    erIkkeSperretAnsatt &&
-                    erIkkeDoed &&
-                    erFerdigBeregnet &&
-                    erIkkeKvp
-        }
-
-        val ukjentAdressebeskyttelse = kandidat.adressebeskyttelse?.let { beskyttelse ->
-            beskyttelse !in listOf("STRENGT_FORTROLIG_UTLAND", "STRENGT_FORTROLIG", "FORTROLIG", "UGRADERT")
-        } ?: false
-
-        val kandidatOgGraderingNull = synligUtenomGradering && kandidat.adressebeskyttelse == null
-        val kandidatOgGraderingUGRADERT = synligUtenomGradering && kandidat.adressebeskyttelse == "UGRADERT"
-        val harGraderinginnslag = kandidat.adressebeskyttelse != null;
-        val stoppetAvFortrolig = synligUtenomGradering && kandidat.adressebeskyttelse == "FORTROLIG"
-        val stoppetAvStrengtFortrolig = synligUtenomGradering && kandidat.adressebeskyttelse == "STRENGT_FORTROLIG"
-        val stoppetAvStrengtFortroligUtland = synligUtenomGradering && kandidat.adressebeskyttelse == "STRENGT_FORTROLIG_UTLAND"
-
-        val uenigOmKode7 = synligUtenomGradering && (
-                (kandidat.adressebeskyttelse != "FORTROLIG" && kandidat.oppfølgingsinformasjon?.diskresjonskode == "7") ||
-                        (kandidat.adressebeskyttelse == "FORTROLIG" && kandidat.oppfølgingsinformasjon?.diskresjonskode != "7")
-                )
-
-        val uenigOmKode6 = synligUtenomGradering && (
-                (kandidat.adressebeskyttelse != "STRENGT_FORTROLIG" && kandidat.oppfølgingsinformasjon?.diskresjonskode == "6") ||
-                        (kandidat.adressebeskyttelse == "STRENGT_FORTROLIG" && kandidat.oppfølgingsinformasjon?.diskresjonskode != "6")
-                )
-
-        val adressebeskyttelseMenStoppetAvAndre = !synligUtenomGradering && kandidat.adressebeskyttelse != null && kandidat.adressebeskyttelse != "UGRADERT"
-
-        val betingelser = listOf(
-            ukjentAdressebeskyttelse,
-            kandidatOgGraderingNull,
-            kandidatOgGraderingUGRADERT,
-            harGraderinginnslag,
-            stoppetAvFortrolig,
-            stoppetAvStrengtFortrolig,
-            stoppetAvStrengtFortroligUtland,
-            uenigOmKode7,
-            uenigOmKode6,
-            adressebeskyttelseMenStoppetAvAndre
-        )
-
-        if (betingelser.any { it }) {
-            secureLog.info("Adressebeskyttelse vurdering for aktørId=${kandidat.aktørId}: " +
-                    "ukjentAdressebeskyttelse=$ukjentAdressebeskyttelse, " +
-                    "kandidatOgGraderingNull=$kandidatOgGraderingNull, " +
-                    "kandidatOgGraderingUGRADERT=$kandidatOgGraderingUGRADERT, " +
-                    "harGraderinginnslag=$harGraderinginnslag, " +
-                    "stoppetAvFortrolig=$stoppetAvFortrolig, " +
-                    "stoppetAvStrengtFortrolig=$stoppetAvStrengtFortrolig, " +
-                    "stoppetAvStrengtFortroligUtland=$stoppetAvStrengtFortroligUtland, " +
-                    "uenigOmKode7KanSkyldesForsinkelse=$uenigOmKode7, " +
-                    "uenigOmKode6KanSkyldesForsinkelse=$uenigOmKode6, " +
-                    "adressebeskyttelseMenIkkeKandidat=$adressebeskyttelseMenStoppetAvAndre"
-            )
-        }
     }
 }
