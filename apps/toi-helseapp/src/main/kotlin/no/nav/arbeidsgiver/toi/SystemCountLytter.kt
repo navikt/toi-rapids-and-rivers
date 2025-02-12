5c9baa8cf220afb193a31c2ef4f9a125bbe6fb18
frodank <11638682+frodank@users.noreply.github.com>
2025-02-12 15:12:17 +0100

    Den lengste kjeden for meldinger er nå på 11
    Co-authored-by: Joar Aurdal <13847640+joarau@users.noreply.github.com>

--- apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/SystemCountLytter.kt
+++ apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/SystemCountLytter.kt
@@ -26,7 +26,7 @@ class SystemCountLytter(rapidsConnection: RapidsConnection): River.PacketListene
         meterRegistry: MeterRegistry
     ) {
         val systemReadCount = packet["system_read_count"].asInt()
-        if(systemReadCount>=10) {
+        if(systemReadCount>11) {
             val eventName = packet["@event_name"].asTextNullable()
             val systemParticipatingServices = packet["system_participating_services"].toPrettyString()
             log.error("System-readcount er stor!! Kan være at hendelser har havnet i loop!!\n" +
