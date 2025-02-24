f765311e83eceaea85f490f13916a00e51059fe5
Vilde Stefferud <vilde.stefferud@nav.no>
2025-02-24 14:06:43 +0100

    Legg til javalin for adressebeskyttelse-endepunkt

---
+++ apps/toi-livshendelse/src/main/kotlin/no/nav/arbeidsgiver/toi/livshendelser/rest/HentAdressebeskyttelse.kt
@@ -0,0 +1,19 @@
+package no.nav.arbeidsgiver.toi.livshendelser.rest
+
+import io.javalin.http.Context
+import no.nav.arbeidsgiver.toi.livshendelser.PdlKlient
+import no.nav.security.token.support.core.configuration.IssuerProperties
+
+
+fun hentAdressebeskyttelse(pdlKlient: PdlKlient, issuerProperties: Map<Rolle, Pair<String, IssuerProperties>>): (Context) -> Unit {
+    return { context ->
+        context.sjekkTilgang(Rolle.VEILEDER, issuerProperties)
+
+        val harAdressebeskyttelse = pdlKlient.diskresjonsHendelseForIdent(context.pathParam("fnr")).any{ it.gradering != "UGRADERT" }
+        context.json(AdressebeskyttelseDTO(harAdressebeskyttelse)).status(200)
+    }
+}
+
+data class AdressebeskyttelseDTO(
+    val harAdressebeskyttelse: Boolean
+)
