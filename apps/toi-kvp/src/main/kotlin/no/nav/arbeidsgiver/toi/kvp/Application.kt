b63a74eb699d0870a5777dc3fe6de754b7e707d0
Are Husby <are.husby@nav.no>
2025-03-25 14:53:23 +0100

    Vær eksplisitt på hvilken logback.xml som brukes
    for å unngå at logging events ment til Securelogs hvaer i ordinær applogg.

--- apps/toi-kvp/src/main/kotlin/no/nav/arbeidsgiver/toi/kvp/Application.kt
+++ apps/toi-kvp/src/main/kotlin/no/nav/arbeidsgiver/toi/kvp/Application.kt
@@ -8,18 +8,48 @@ import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-fun main() = RapidApplication.create(System.getenv()).also { rapidsConnection ->
-    KvpLytter(rapidsConnection)
-}.start()
+private val log = noClassLogger()
+val secureLog = LoggerFactory.getLogger("secureLog")!!
+
+fun main() {
+    log.info("Starter app.")
+    secureLog.info("Starter app. Dette er ment å logges til Securelogs. Hvis du ser dette i den ordinære apploggen er noe galt, og sensitive data kan havne i feil logg.")
+
+    RapidApplication.create(System.getenv()).also { rapidsConnection ->
+        KvpLytter(rapidsConnection)
+    }.start()
+}
 
 val Any.log: Logger
     get() = LoggerFactory.getLogger(this::class.java)
 
-val secureLog = LoggerFactory.getLogger("secureLog")
 
 fun JsonMessage.fjernMetadataOgKonverter(): JsonNode {
     val jsonNode = jacksonObjectMapper().readTree(this.toJson()) as ObjectNode
-    val metadataFelter = listOf("system_read_count", "system_participating_services", "@event_name", "@id", "@opprettet")
+    val metadataFelter =
+        listOf("system_read_count", "system_participating_services", "@event_name", "@id", "@opprettet")
     jsonNode.remove(metadataFelter)
     return jsonNode
+}
+
+
+/**
+ * Convenience for å slippe å skrive eksplistt navn på Logger når Logger opprettes. Ment å tilsvare Java-måten, hvor
+ * Loggernavnet pleier å være pakkenavn+klassenavn på den loggende koden.
+ * Brukes til å logging fra Kotlin-kode hvor vi ikke er inne i en klasse, typisk i en "top level function".
+ * Kalles fra den filen du ønsker å logg i slik:
+ *```
+ * import no.nav.yada.no.nav.toi.noClassLogger
+ * private val no.nav.toi.log: Logger = no.nav.toi.noClassLogger()
+ * fun myTopLevelFunction() {
+ *      no.nav.toi.log.info("yada yada yada")
+ *      ...
+ * }
+ *```
+ *
+ *@return En Logger hvor navnet er sammensatt av pakkenavnet og filnavnet til den kallende koden
+ */
+fun noClassLogger(): Logger {
+    val callerClassName = Throwable().stackTrace[1].className
+    return LoggerFactory.getLogger(callerClassName)
 }
