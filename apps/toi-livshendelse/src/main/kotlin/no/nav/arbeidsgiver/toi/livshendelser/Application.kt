c902be3bc634cb979397b11854df9fb116ef3b78
Vilde Stefferud <33753494+vildestefferud@users.noreply.github.com>
2025-03-26 13:22:17 +0100

    Restendepunkt adressebeskyttelse (#103)
    * Legg til javalin, jackson og tokenvalidation bibliotek
    
    * Flytt funksjoner for å lettere akksesere dem
    
    * Gjenbruk sikkerhet fra toi-synlighetsmotor
    
    * Legg til javalin for adressebeskyttelse-endepunkt
    
    * Legg til testoppsett for hentadressebeskyttelse rest endepunkt
    
    * Legg på assertions på rest test for adressebeskyttelse
    
    * Skrur opp min-replicas siden tjeneste skal bli rest-tjeneste
    
    Co-authored-by: Vilde Stefferud <33753494+vildestefferud@users.noreply.github.com>
    
    * Fiks port og mock-problemer
    
    Co-authored-by: Vilde Stefferud <33753494+vildestefferud@users.noreply.github.com>
    
    * Rename funksjon til å bedre beskrive hva den returnerer og fiks test
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Lag test for person uten adressebeskyttelse
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Lag test for at token mangler
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Lag test for når token har utgått
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Lag test for når token har feil issuer
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Lag test for at token må ha Nav-ident
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Lag test for at token må ha rett audience
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Lag test for at token må ha rett algoritme
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Fjern ubrukt import
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    * Flytt fnr inn i body og endre til post-kall
    
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Joar Aurdal <13847640+joarau@users.noreply.github.com>
    Co-authored-by: Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>

--- apps/toi-livshendelse/src/main/kotlin/no/nav/arbeidsgiver/toi/livshendelser/Application.kt
+++ apps/toi-livshendelse/src/main/kotlin/no/nav/arbeidsgiver/toi/livshendelser/Application.kt
@@ -2,18 +2,26 @@ package no.nav.arbeidsgiver.toi.livshendelser
 
 import AdressebeskyttelseLytter
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
+import io.javalin.Javalin
+import no.nav.arbeidsgiver.toi.livshendelser.rest.harAdressebeskyttelse
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.person.pdl.leesah.Personhendelse
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.system.exitProcess
+import no.nav.arbeidsgiver.toi.livshendelser.rest.*
+import no.nav.security.token.support.core.configuration.IssuerProperties
 
 private val env = System.getenv()
 
 private val log = noClassLogger()
 private val secureLog = LoggerFactory.getLogger("secureLog")
 
+fun opprettJavalinMedTilgangskontroll(port: Int): Javalin =
+    Javalin.create {
+        it.http.defaultContentType = "application/json"
+    }.start(port)
 
 fun main() {
     log.info("Starter app.")
@@ -22,7 +30,9 @@ fun main() {
     try {
         startApp(
             rapidsConnection(),
-            PdlKlient(env["PDL_URL"]!!, AccessTokenClient(env))
+            PdlKlient(env["PDL_URL"]!!, AccessTokenClient(env)),
+            opprettJavalinMedTilgangskontroll(8080),
+            hentIssuerProperties(env)
         )
     } catch (e: Exception) {
         secureLog.error("Uhåndtert exception, stanser applikasjonen", e)
@@ -33,8 +43,13 @@ fun main() {
 
 fun startApp(
     rapidsConnection: RapidsConnection,
-    pdlKlient: PdlKlient
-) {
+    pdlKlient: PdlKlient,
+    javalin: Javalin,
+    issuerProperties: Map<Rolle, Pair<String, IssuerProperties>>,
+
+    ) {
+    javalin.post("/adressebeskyttelse", harAdressebeskyttelse(pdlKlient, issuerProperties))
+
     val log = LoggerFactory.getLogger("Application.kt")
     try {
         rapidsConnection.also {
