9b089380d532eb25aefbc7f22abe2fded174ad15
Are Husby <are.husby@nav.no>
2025-03-26 15:17:18 +0100

    Vær eksplisitt på hvilken logback.xml som brukes
    for å unngå at logging events ment til Securelogs havner i ordinær applogg.

--- apps/toi-organisasjonsenhet/src/main/kotlin/no/nav/arbeidsgiver/toi/organisasjonsenhet/Application.kt
+++ apps/toi-organisasjonsenhet/src/main/kotlin/no/nav/arbeidsgiver/toi/organisasjonsenhet/Application.kt
@@ -5,7 +5,15 @@ import no.nav.helse.rapids_rivers.RapidApplication
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-fun main() = startApp(Norg2Klient(norg2Url()), RapidApplication.create(System.getenv()))
+private val log = noClassLogger()
+val secureLog = LoggerFactory.getLogger("secureLog")!!
+
+fun main() {
+    log.info("Starter app.")
+    secureLog.info("Starter app. Dette er ment å logges til Securelogs. Hvis du ser dette i den ordinære apploggen er noe galt, og sensitive data kan havne i feil logg.")
+
+    startApp(Norg2Klient(norg2Url()), RapidApplication.create(System.getenv()))
+}
 
 fun startApp(norg2Klient: Norg2Klient, rapidsConnection: RapidsConnection) = rapidsConnection.also {
     OrganisasjonsenhetLytter(norg2Klient, rapidsConnection)
@@ -13,5 +21,26 @@ fun startApp(norg2Klient: Norg2Klient, rapidsConnection: RapidsConnection) = rap
 
 val Any.log: Logger
     get() = LoggerFactory.getLogger(this::class.java)
+
+/**
+ * Convenience for å slippe å skrive eksplistt navn på Logger når Logger opprettes. Ment å tilsvare Java-måten, hvor
+ * Loggernavnet pleier å være pakkenavn+klassenavn på den loggende koden.
+ * Brukes til å logging fra Kotlin-kode hvor vi ikke er inne i en klasse, typisk i en "top level function".
+ * Kalles fra den filen du ønsker å logg i slik:
+ *```
+ * import no.nav.yada.no.nav.toi.noClassLogger
+ * private val no.nav.toi.log: Logger = no.nav.toi.noClassLogger()
+ * fun myTopLevelFunction() {
+ *      no.nav.toi.log.info("yada yada yada")
+ *      ...
+ * }
+ *```
+ *
+ *@return En Logger hvor navnet er sammensatt av pakkenavnet og filnavnet til den kallende koden
+ */
+fun noClassLogger(): Logger {
+    val callerClassName = Throwable().stackTrace[1].className
+    return LoggerFactory.getLogger(callerClassName)
+}
 
 private fun norg2Url() = System.getenv("NORG2_URL") ?: throw Exception("Mangler NORG2_URL")
