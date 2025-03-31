466925160cb956c9407aa5f38de13521682dc924
Frode Ankill Kämpe <11638682+frodank@users.noreply.github.com>
2025-03-31 16:55:14 +0200

    Fikse tester til å dra opp database og legge på lytter

--- apps/toi-arbeidssoekeropplysninger/src/test/kotlin/no/nav/arbeidsgiver/toi/arbeidssoekeropplysninger/ArbeissoekeropplysningerBehovTest.kt
+++ apps/toi-arbeidssoekeropplysninger/src/test/kotlin/no/nav/arbeidsgiver/toi/arbeidssoekeropplysninger/ArbeissoekeropplysningerBehovTest.kt
@@ -1,16 +1,88 @@
 package no.nav.arbeidsgiver.toi.arbeidssoekeropplysninger
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.navikt.tbd_libs.rapids_and_rivers.test_support.TestRapid
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.utility.DockerImageName
+import java.time.ZonedDateTime
+import java.util.*
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ArbeissoekeropplysningerBehovTest {
 
     private val testRapid = TestRapid()
+    private val aktørId = "123123123"
+
+    private val localEnv = mutableMapOf<String, String>(
+        "DB_DATABASE" to "test",
+        "DB_USERNAME" to "test",
+        "DB_PASSWORD" to "test"
+    )
+    val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+    val localPostgres = PostgreSQLContainer(DockerImageName.parse("postgres:17-alpine"))
+        .waitingFor(Wait.forListeningPort())
+        .apply { start() }
+        .also { localConfig ->
+            localEnv["DB_HOST"] = localConfig.host
+            localEnv["DB_PORT"] = localConfig.getMappedPort(5432).toString()
+        }
+
+    lateinit var repository: Repository
+
+    private val jacksonMapper = jacksonObjectMapper()
+        .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .registerModule(JavaTimeModule())
+
+    @BeforeAll
+    fun init() {
+        val databaseConfig = DatabaseConfig(localEnv, meterRegistry)
+        val dataSource = databaseConfig.lagDatasource()
+        kjørFlywayMigreringer(dataSource)
+
+        repository = Repository(dataSource)
+        val periode = jacksonMapper.valueToTree<JsonNode>(Periode(UUID.randomUUID(), aktørId, aktørId, ZonedDateTime.now(), null))
+        repository.lagreOppfølgingsperiodemelding(periode)
+        ArbeidssoekeropplysningerBehovLytter(testRapid, repository)
+        ArbeidssoekerperiodeRapidLytter(testRapid, repository)
+    }
+
+    @AfterAll
+    fun teardown() {
+        localPostgres.close()
+    }
+
+    @AfterEach
+    fun cleanup() {
+        testRapid.reset()
+    }
+
     @Test
     fun `legg på svar om første behov er arbeidssokeropplysninger`() {
-        val aktørId = "123123123"
+        testRapid.sendTestMessage(behovsMelding(ident = aktørId, behovListe = """["arbeidssokeropplysninger"]"""))
+
+        val inspektør = testRapid.inspektør
+
+        assertThat(inspektør.size).isEqualTo(1)
+        val message = inspektør.message(0)
+        message.assertHarArbeidssokeropplysninger()
+    }
+
+    @Test
+    fun `legg på null om det ikke finnes arbeidssokeropplysninger på person og første behov er arbeidssokeropplysninger`() {
+        val aktørId = "finnes ikke i database"
         testRapid.sendTestMessage(behovsMelding(ident = aktørId, behovListe = """["arbeidssokeropplysninger"]"""))
 
         val inspektør = testRapid.inspektør
@@ -22,7 +94,6 @@ class ArbeissoekeropplysningerBehovTest {
 
     @Test
     fun `ikke legg på svar om andre uløste behov enn arbeidssokeropplysninger er først i listen`() {
-        val aktørId = "123123123"
         testRapid.sendTestMessage(behovsMelding(ident = aktørId, behovListe = """["noeannet", "arbeidssokeropplysninger"]"""))
 
         val inspektør = testRapid.inspektør
@@ -32,7 +103,6 @@ class ArbeissoekeropplysningerBehovTest {
 
     @Test
     fun `legg på svar om behov nummer 2 er arbeidssokeropplysninger, dersom første behov har en løsning`() {
-        val aktørId = "123123123"
         testRapid.sendTestMessage(
             behovsMelding(
                 ident = aktørId,
@@ -74,8 +144,6 @@ class ArbeissoekeropplysningerBehovTest {
 
     @Test
     fun `legg på svar om behov nummer 2 er arbeidssokeropplysninger, dersom første behov har en løsning med null-verdi`() {
-        val aktørId = "123123123"
-
         testRapid.sendTestMessage(
             behovsMelding(
                 ident = aktørId,
