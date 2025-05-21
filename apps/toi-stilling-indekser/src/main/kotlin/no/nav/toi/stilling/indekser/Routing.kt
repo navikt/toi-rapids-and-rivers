24d3f3e0d885f57edc04f27e0b4352729723d51c
haugse <sara.haugse@gmail.com>
2025-05-21 15:06:46 +0200

    Endre litt i respons fra endepunktet

--- apps/toi-stilling-indekser/src/main/kotlin/no/nav/toi/stilling/indekser/Routing.kt
+++ apps/toi-stilling-indekser/src/main/kotlin/no/nav/toi/stilling/indekser/Routing.kt
@@ -1,14 +1,14 @@
 package no.nav.toi.stilling.indekser
 
 import io.ktor.http.*
-import io.ktor.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         get("/isalive") {
-            if(Liveness.isAlive) VersionCheckResult.OK else call.response.status(HttpStatusCode.ServiceUnavailable)
+            if(Liveness.isAlive) call.respondText("ALIVE", status = HttpStatusCode.OK) else call.respond(HttpStatusCode.ServiceUnavailable)
         }
     }
 }
