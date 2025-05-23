eb97927560f38792022e2f3a0dfa079c9799e9a3
haugse <sara.haugse@gmail.com>
2025-05-23 10:45:12 +0200

    legg på logg om isalive blir kalt

--- apps/toi-stilling-indekser/src/main/kotlin/no/nav/toi/stilling/indekser/Routing.kt
+++ apps/toi-stilling-indekser/src/main/kotlin/no/nav/toi/stilling/indekser/Routing.kt
@@ -8,6 +8,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/isalive") {
+            log.info("iaalive kalt")
             if((!Liveness.isAlive && !rapidIsAlive.invoke())) {
                 log.info("Appen skal starte på nytt, isalive er false")
             }
