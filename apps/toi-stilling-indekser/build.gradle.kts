f1bb64209138c200da6a90d608d4689848e39217
haugse <sara.haugse@gmail.com>
2025-05-23 10:19:14 +0200

    Bare runtime på opentelemetry

--- apps/toi-stilling-indekser/build.gradle.kts
+++ apps/toi-stilling-indekser/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("io.confluent:kafka-avro-serializer:7.8.0")
     implementation("org.opensearch.client:opensearch-java:2.22.0")
     implementation("org.apache.httpcomponents.client5:httpclient5:5.4.2")
-    implementation("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:2.16.0-alpha")
+    runtimeOnly("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:2.16.0-alpha")
 
     testImplementation("org.opensearch:opensearch-testcontainers:2.1.2")
     testImplementation("org.testcontainers:kafka:1.20.3")
