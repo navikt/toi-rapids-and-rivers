536c5560eef5d859dedaa57f0c178c569cf6083d
Ken Ove Andersen <ken.ove.andersen@nav.no>
2025-06-02 10:22:32 +0200

    Revert "Endre datoer til strenger da frontend forventer det slik"
    This reverts commit 1995718194fe0a850dec95872fbea00be0abf80d.

--- apps/toi-stilling-indekser/src/main/kotlin/no/nav/toi/stilling/indekser/eksternLytter/KonverterTilStilling.kt
+++ apps/toi-stilling-indekser/src/main/kotlin/no/nav/toi/stilling/indekser/eksternLytter/KonverterTilStilling.kt
@@ -18,11 +18,11 @@ fun konverterTilStilling(ad: Ad): Stilling {
         annonsenr = ad.adnr,
         status = ad.status.name,
         privacy = ad.privacy.name,
-        published = if(ad.published.isNullOrBlank()) null else konverterDato(ad.published).toString(),
+        published = if(ad.published.isNullOrBlank()) null else konverterDato(ad.published),
         publishedByAdmin = ad.publishedByAdmin,
-        expires = if(ad.expires.isNullOrBlank()) null else konverterDato(ad.expires).toString(),
-        created = konverterDato(ad.created).toString(),
-        updated = konverterDato(ad.updated).toString(),
+        expires = if(ad.expires.isNullOrBlank()) null else konverterDato(ad.expires),
+        created = konverterDato(ad.created),
+        updated = konverterDato(ad.updated),
         employer = ad.employer?.let {
             DirektemeldtStillingArbeidsgiver(
                 name = it.name,
