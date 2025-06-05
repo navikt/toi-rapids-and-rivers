bf0878292b2faf2c35f3f50b9e8c93c39d86347e
Frode.Ankill.Kampe <11638682+frodank@users.noreply.github.com>
2025-06-05 13:35:05 +0200

    indekserDirektemeldtStilling-hendelser fyres av med manuell endring av direktemeldte stillinger, som gjerne gjøres på dagtid

--- apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/eventsjekk.kt
+++ apps/toi-helseapp/src/main/kotlin/no/nav/arbeidsgiver/toi/eventsjekk.kt
@@ -110,6 +110,7 @@ class SisteEvent(private val eventName: String, private val duration: Duration) 
             "siste14avedtak" -> justertGrenseverdiForAlarm(Duration.ofHours(3))
             "arbeidsgiversKandidatliste.VisningKontaktinfo" -> justertGrenseverdiForAlarm(Duration.ofHours(3))
             "notifikasjon.cv-delt" -> justertGrenseverdiForAlarm(Duration.ofHours(2))
+            "indekserDirektemeldtStilling" -> justertGrenseverdiForAlarm(Duration.ofHours(2))
             else -> Duration.ofHours(1)
         }
 
