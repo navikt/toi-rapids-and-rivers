0b64fc69d816c816c584e5b2f11c2338f7a0b356
Ken Ove Andersen <ken.ove.andersen@nav.no>
2025-09-11 12:33:36 +0200

    Fiks koden etter merge
    Co-authored-by: Sara <43448582+haugse@users.noreply.github.com>

--- apps/toi-publisering-til-arbeidsplassen/src/main/kotlin/no/nav/toi/stilling/publiser/arbeidsplassen/Application.kt
+++ apps/toi-publisering-til-arbeidsplassen/src/main/kotlin/no/nav/toi/stilling/publiser/arbeidsplassen/Application.kt
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import no.nav.helse.rapids_rivers.RapidApplication
-import no.nav.toi.stilling.publiser.arbeidsplassen.SecureLogLogger.Companion.secure
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.net.URI
@@ -14,12 +13,13 @@ private val log = noClassLogger()
 
 fun main() {
     log.info("Starter app.")
-    secure(log).info("Starter app. Dette er ment å logges til Securelogs. Hvis du ser dette i den ordinære apploggen er noe galt, og sensitive data kan havne i feil logg.")
 
     val env = System.getenv()
     val importApiBaseUrl = URI(env.getValue("IMPORT_API_BASE_URL") ?: error("IMPORT_API_BASE_URL mangler"))
     val token = env.getValue("ARBEIDSPLASSEN_IMPORTAPI_TOKEN") ?: error("ARBEIDSPLASSEN_IMPORTAPI_TOKEN mangler")
     val arbeidsplassenRestKlient = ArbeidsplassenRestKlientImpl(importApiBaseUrl, token)
+    startApp(rapidsConnection(env), arbeidsplassenRestKlient)
+}
 
 fun startApp(rapid: RapidsConnection, arbeidsplassenRestKlient: ArbeidsplassenRestKlient) = rapid.also {
     StillingTilArbeidsplassenLytter(rapid, arbeidsplassenRestKlient)
@@ -54,85 +54,6 @@ val objectMapper = jacksonObjectMapper()
 fun noClassLogger(): Logger {
     val callerClassName = Throwable().stackTrace[1].className
     return LoggerFactory.getLogger(callerClassName)
-}
-
-/**
- * Styrer logging til [secureLog](https://doc.nais.io/observability/logs/#secure-logs), forutsatt riktig konfigurasjon av Logback.
- *
- * Brukes ved å dekorere en hvilken som helst, vanlig org.slf4j.Logger slik:
- * ```
- * import no.nav.statistikkapi.logging.no.nav.toi.SecureLogLogger.Companion.secure
- * ...
- * secure(no.nav.toi.log).info(msg)
- * ```
- *
- * For at dette skal virke må appens fil `logback.xml` bruke appendere med filtere slik at logging events som har en marker med navn `SECURE_LOG` styres til riktig loggfil:
- * ```
- * <configuration>
- *     <appender name="appLog" ...>
- *         ...
- *         <filter class="ch.qos.logback.core.filter.EvaluatorFilter">
- *             <evaluator class="ch.qos.logback.classic.boolex.OnMarkerEvaluator">
- *                 <marker>SECURE_LOG</marker>
- *             </evaluator>
- *             <OnMismatch>NEUTRAL</OnMismatch>
- *             <OnMatch>DENY</OnMatch>
- *         </filter>
- *     </appender>
- *
- *     <appender name="secureLog" ...>
- *         ...
- *         <filter class="ch.qos.logback.core.filter.EvaluatorFilter">
- *             <evaluator class="ch.qos.logback.classic.boolex.OnMarkerEvaluator">
- *                 <marker>SECURE_LOG</marker>
- *             </evaluator>
- *             <OnMismatch>DENY</OnMismatch>
- *             <OnMatch>NEUTRAL</OnMatch>
- *         </filter>
- *     </appender>
- *
- *     <root ...>
- *         <appender-ref ref="appLog"/>
- *         <appender-ref ref="secureLog"/>
- *     </root>
- * </configuration>
- * ```
- * Se [offisiell Logback-dokumentasjon](https://logback.qos.ch/manual/filters.html#evaluatorFilter)
- *
- */
-class SecureLogLogger private constructor(private val l: Logger) {
-
-    val markerName: String = "SECURE_LOG"
-
-    private val m: Marker = MarkerFactory.getMarker(markerName)
-
-    fun info(msg: String) {
-        l.info(m, msg)
-    }
-
-    fun info(msg: String, t: Throwable) {
-        l.info(m, msg, t)
-    }
-
-    fun warn(msg: String) {
-        l.warn(m, msg)
-    }
-
-    fun warn(msg: String, t: Throwable) {
-        l.warn(m, msg, t)
-    }
-
-    fun error(msg: String) {
-        l.error(m, msg)
-    }
-
-    fun error(msg: String, t: Throwable) {
-        l.error(m, msg, t)
-    }
-
-    companion object {
-        fun secure(l: Logger): SecureLogLogger = SecureLogLogger(l)
-    }
 }
 
 
