c6556b0110f1fdf9beea5c3498fb89c04e3ee8f5
Frode.Ankill.Kampe <11638682+frodank@users.noreply.github.com>
2026-02-04 18:05:37 +0100

    ktor allerede brakt inn

--- apps/toi-siste-oppfolgingsperiode/build.gradle.kts
+++ apps/toi-siste-oppfolgingsperiode/build.gradle.kts
@@ -4,7 +4,4 @@ plugins {
 
 dependencies {
     implementation("org.apache.kafka:kafka-streams:4.1.0")
-
-    implementation("io.ktor:ktor-server-netty:3.2.3")
-    implementation("io.ktor:ktor-server-core:3.2.3")
 }
