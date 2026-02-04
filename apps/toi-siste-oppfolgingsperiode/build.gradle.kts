96758d552891cd2e184d883b6f0ef5de7b6801e3
Frode.Ankill.Kampe <11638682+frodank@users.noreply.github.com>
2026-02-04 01:05:55 +0100

    Fiks rett versjon

--- apps/toi-siste-oppfolgingsperiode/build.gradle.kts
+++ apps/toi-siste-oppfolgingsperiode/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 dependencies {
     implementation("org.apache.kafka:kafka-streams:4.1.0")
 
-    implementation("io.ktor:ktor-server-netty:2.3.7")
-    implementation("io.ktor:ktor-server-core:2.3.7")
+    implementation("io.ktor:ktor-server-netty:3.2.3")
+    implementation("io.ktor:ktor-server-core:3.2.3")
 }
