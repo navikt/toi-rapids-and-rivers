b449ee3b40d32bf87d9b659dd17e06646c2cd1b2
Are Husby <are.husby@nav.no>
2026-04-15 17:24:56 +0200

    Oppgrader testcontainer versjoner
    utløst av at bygging av main-branch feilet på Linux utviklermaskin

--- apps/toi-stilling-indekser/build.gradle.kts
+++ apps/toi-stilling-indekser/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 }
 
 dependencies {
+    testImplementation(platform("org.testcontainers:testcontainers-bom:2.0.4"))
+
     implementation("org.apache.avro:avro:1.12.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.19.0")
     implementation("io.confluent:kafka-avro-serializer:7.8.0")
@@ -11,8 +13,8 @@ dependencies {
     implementation("org.apache.httpcomponents.client5:httpclient5:5.4.2")
     runtimeOnly("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:2.16.0-alpha")
 
-    testImplementation("org.testcontainers:elasticsearch:1.21.4")
-    testImplementation("org.testcontainers:kafka:1.21.4")
-    testImplementation("org.testcontainers:junit-jupiter:1.21.4")
+    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("org.testcontainers:testcontainers-elasticsearch")
+    testImplementation("org.testcontainers:testcontainers-kafka")
     testImplementation("io.mockk:mockk:1.13.17")
 }
