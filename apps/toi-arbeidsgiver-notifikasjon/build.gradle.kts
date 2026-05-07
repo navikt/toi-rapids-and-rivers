3d9702e478445ec3632e79d8495a8ad9a3e634ee
Are Husby <are.husby@nav.no>
2026-05-07 15:20:51 +0200

    Fjern unødvendig dependecy

--- apps/toi-arbeidsgiver-notifikasjon/build.gradle.kts
+++ apps/toi-arbeidsgiver-notifikasjon/build.gradle.kts
@@ -7,8 +7,6 @@ dependencies {
 
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.github.kittinunf.fuel:fuel-jackson:2.3.1")
-
-    implementation("no.nav.security:token-client-core:2.1.0")
-
+    
     testImplementation("org.wiremock:wiremock-standalone:3.0.4")
 }
