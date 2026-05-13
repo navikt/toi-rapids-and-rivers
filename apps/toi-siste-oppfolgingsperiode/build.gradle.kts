c35e5e307ddb7151cc1a357086c2ab63bbf0aa6f
Frode.Ankill.Kampe <11638682+frodank@users.noreply.github.com>
2026-05-13 12:19:52 +0200

    Critical vulnerability

--- apps/toi-siste-oppfolgingsperiode/build.gradle.kts
+++ apps/toi-siste-oppfolgingsperiode/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 
 dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.0")
-    implementation("org.apache.kafka:kafka-streams:4.1.0")
+    implementation("org.apache.kafka:kafka-streams:4.2.0")
     implementation("io.javalin:javalin:6.7.0")
 }
