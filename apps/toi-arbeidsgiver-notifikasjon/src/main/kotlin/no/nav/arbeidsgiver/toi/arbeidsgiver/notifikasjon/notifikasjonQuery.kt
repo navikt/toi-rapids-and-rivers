6568447e819cd841a88543dcf3233f85d23b56cb
Frode.Ankill.Kampe <11638682+frodank@users.noreply.github.com>
2026-06-10 11:01:20 +0200

    Trunkerer titler med mer enn 140 karakterer, siden stillinger fra arbeidsplassen kan ha ekstra lange titler.

--- apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/notifikasjonQuery.kt
+++ apps/toi-arbeidsgiver-notifikasjon/src/main/kotlin/no/nav/arbeidsgiver/toi/arbeidsgiver/notifikasjon/notifikasjonQuery.kt
@@ -159,7 +159,7 @@ fun queryOpprettNySak(
     return opprettQuery(query, mapOf(
         "grupperingsid" to stillingsId.toString(),
         "virksomhetsnummer" to organisasjonsnummer,
-        "tittel" to stillingstittel.escapeJson(),
+        "tittel" to stillingstittel.escapeJson().take(140),
         "lenke" to lenkeTilStilling,
         "merkelapp" to SAK_MERKELAPP,
         "initiellStatus" to "MOTTATT",
