256c1d01b1603a8fc6f7dfb1e8e0bd0211a38b0c
Are Husby <are.husby@nav.no>
2026-06-16 15:29:18 +0200

    Vær eksplisitt på dependency version require vs stricty/preferred

--- buildSrc/src/main/kotlin/toi.common.gradle.kts
+++ buildSrc/src/main/kotlin/toi.common.gradle.kts
@@ -15,14 +15,33 @@ repositories {
 
 dependencies {
     constraints {
-        implementation("org.slf4j:slf4j-api:2.0.18")
-        implementation("ch.qos.logback:logback-classic:1.5.23")
-        implementation("net.logstash.logback:logstash-logback-encoder:9.0")
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-        testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
-        testImplementation("org.assertj:assertj-core:3.23.1")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.1")
+        // Alternativer til require er strictly og preferred.
+        // Se https://docs.gradle.org/current/userguide/dependency_versions.html#sec:rich-version-constraints
+
+        implementation("org.slf4j:slf4j-api") {
+            version { require("2.0.18") }
+        }
+        implementation("ch.qos.logback:logback-classic") {
+            version { require("1.5.23") }
+        }
+        implementation("net.logstash.logback:logstash-logback-encoder") {
+            version { require("9.0") }
+        }
+        testImplementation("org.junit.jupiter:junit-jupiter-api") {
+            version { require("5.9.1") }
+        }
+        testImplementation("org.junit.jupiter:junit-jupiter-params") {
+            version { require("5.9.1") }
+        }
+        testImplementation("org.assertj:assertj-core") {
+            version { require("3.23.1") }
+        }
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine") {
+            version { require("5.9.1") }
+        }
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher") {
+            version { require("1.9.1") }
+        }
     }
 
     implementation(kotlin("stdlib"))
